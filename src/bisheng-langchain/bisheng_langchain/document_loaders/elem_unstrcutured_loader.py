8067aa1a92b13e0410b2b1911fe653805de812c2
Feng Han <hanfeng@dataelem.com>
2023-09-17 20:44:34 +0800

    support ElemUnstructuredLoaderV0

--- src/bisheng-langchain/bisheng_langchain/document_loaders/elem_unstrcutured_loader.py
+++ src/bisheng-langchain/bisheng_langchain/document_loaders/elem_unstrcutured_loader.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 """Loads PDF with semantic splilter."""
+import base64
 import io
 import json
 import logging
@@ -62,5 +63,42 @@ class ElemUnstructuredLoader(BasePDFLoader):
             'start': start, 'end': end, 'chunk_type': chunk_type,
             'token_to_bbox': token_to_bbox
         }
+        doc = Document(page_content=page_content, metadata=meta)
+        return [doc]
+
+
+class ElemUnstructuredLoaderV0(BasePDFLoader):
+    """Loads a PDF with pypdf and chunks at character level. dummy version
+
+    Loader also stores page numbers in metadata.
+    """
+    def __init__(self,
+                 file_path: str,
+                 unstructured_api_key: str = None,
+                 unstructured_api_url: str = None,
+                 start: int = 0,
+                 n: int = None,
+                 verbose: bool = False) -> None:
+        """Initialize with a file path."""
+        self.unstructured_api_url = unstructured_api_url
+        self.unstructured_api_key = unstructured_api_key
+        self.headers = {'Content-Type': 'application/json'}
+        super().__init__(file_path)
+
+    def load(self) -> List[Document]:
+        filename = self.file_path
+        b64_data = base64.b64encode(open(filename, 'rb').read()).decode()
+        payload = dict(
+            filename=os.path.basename(filename),
+            b64_data=[b64_data],
+            mode='text')
+
+        resp = requests.post(
+            self.unstructured_api_url,
+            headers=self.headers,
+            json=payload).json()
+
+        page_content = resp['text']
+        meta = {'source': filename}
         doc = Document(page_content=page_content, metadata=meta)
         return [doc]
--- src/bisheng-langchain/tests/test_elem_unstructured_loader.py
+++ src/bisheng-langchain/tests/test_elem_unstructured_loader.py
@@ -1,4 +1,5 @@
 from bisheng_langchain.document_loaders import ElemUnstructuredLoader
+from bisheng_langchain.document_loaders.elem_unstrcutured_loader import ElemUnstructuredLoaderV0
 
 
 def test_loader():
@@ -7,4 +8,15 @@ def test_loader():
     print('docs', docs)
 
 
-test_loader()
+def test_elem_loader2():
+    url = 'http://192.168.106.12:10001/v1/etl4llm/predict'
+    loader = ElemUnstructuredLoaderV0(
+        file_path='./data/毛泽东课件.pptx',
+        unstructured_api_url=url)
+
+    docs = loader.load()
+    print('docs', docs)
+
+
+# test_loader()
+test_elem_loader2()
