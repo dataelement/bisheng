117ff5925f045697bfe1315e5ec82424163345fa
hanfeng <hanfeng@dataelem.com>
2023-10-10 11:57:30 +0800

    update custom llm chat

--- src/bisheng-langchain/bisheng_langchain/chat_models/__init__.py
+++ src/bisheng-langchain/bisheng_langchain/chat_models/__init__.py
@@ -1,4 +1,4 @@
-from .host_llm import HostBaichuanChat, HostChatGLM2, HostLlama2Chat, HostQwenChat
+from .host_llm import CustomLLMChat, HostBaichuanChat, HostChatGLM2, HostLlama2Chat, HostQwenChat
 from .minimax import ChatMinimaxAI
 from .proxy_llm import ProxyChatLLM
 from .wenxin import ChatWenxin
@@ -8,5 +8,6 @@ from .zhipuai import ChatZhipuAI
 __all__ = [
     'ProxyChatLLM', 'ChatMinimaxAI', 'ChatWenxin', 'ChatZhipuAI',
     'ChatXunfeiAI',
-    'HostChatGLM2', 'HostBaichuanChat', 'HostLlama2Chat', 'HostQwenChat'
+    'HostChatGLM2', 'HostBaichuanChat', 'HostLlama2Chat', 'HostQwenChat',
+    'CustomLLMChat'
 ]
