9323a85d1843ee9fbd7f8b148c8c1bf00e8a9391
yaojin <yaojin@dataelem.com>
2023-10-17 11:44:27 +0800

    add ci support multi platform

--- src/bisheng-langchain/bisheng_langchain/document_loaders/__init__.py
+++ src/bisheng-langchain/bisheng_langchain/document_loaders/__init__.py
@@ -2,5 +2,7 @@ from .elem_pdf import PDFWithSemanticLoader
 from .elem_unstrcutured_loader import ElemUnstructuredLoader, ElemUnstructuredLoaderV0
 
 __all__ = [
-    'PDFWithSemanticLoader', 'ElemUnstructuredLoader', 'ElemUnstructuredLoaderV0',
+    'PDFWithSemanticLoader',
+    'ElemUnstructuredLoader',
+    'ElemUnstructuredLoaderV0',
 ]
