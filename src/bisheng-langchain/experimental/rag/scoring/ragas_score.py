f33685bb650616a9e00e0c4579887df8b4cb4aa1
gulixin0922 <gulixin@dataelem.com>
2024-01-27 10:42:51 +0000

    update

--- src/bisheng-langchain/experimental/rag/scoring/ragas_score.py
+++ src/bisheng-langchain/experimental/rag/scoring/ragas_score.py
@@ -1,21 +1,23 @@
-import os
 import json
-import pandas as pd
+import os
 import re
+import shutil
 from collections import defaultdict
+from pathlib import Path
+
+import pandas as pd
+from datasets import Dataset
 from ragas import evaluate
 from ragas.metrics import AnswerCorrectness
-from datasets import Dataset
 
 
 def rages_answer_correctness(dataset):
     # answer_correctness, 只考虑事实相似度
     weights = [1.0, 0.0]
     batch_size = 5
-    answer_correctness = AnswerCorrectness(weights=weights, 
-                                           batch_size=batch_size)
+    answer_correctness = AnswerCorrectness(weights=weights, batch_size=batch_size)
     result = evaluate(
-        dataset = dataset, 
+        dataset=dataset,
         metrics=[
             answer_correctness,
         ],
@@ -25,9 +27,13 @@ def rages_answer_correctness(dataset):
 
 
 def rag_benchmark_scoring(excel_file):
+    if not os.path.exists(excel_file + '.bak'):
+        shutil.copy(excel_file, excel_file + '.bak')
+
     df = pd.read_excel(excel_file)
+    df.dropna(subset=['问题', 'GT', 'rag_answer'], inplace=True)
     all_questions_info = df.to_dict('records')
-    
+
     questions = []
     ground_truths = []
     answers = []
@@ -43,58 +49,43 @@ def rag_benchmark_scoring(excel_file):
         # for i in match:
         #     str_temp = f"【{i}†source】"
         #     pred = pred.replace(str_temp, '')
-        
+
         questions.append(question)
         answers.append(pred)
         ground_truths.append([gt])
         contexts.append([''])
-    
-     # To dict
+
+    # To dict
     data = {
         "question": questions,
         "answer": answers,
         "contexts": contexts,
-        "ground_truths": ground_truths
+        "ground_truths": ground_truths,
     }
     # Convert dict to dataset
     dataset = Dataset.from_dict(data)
 
     answer_correctness_score = rages_answer_correctness(dataset)
-    answer_correctness_score = answer_correctness_score.to_dict('records')
-
     score_map = {
         'answer_correctness': answer_correctness_score,
     }
-
-    ragas_score = defaultdict(lambda: defaultdict(list))
-    for score_type in score_map:
-        for i in range(len(all_questions_info)):
-            if all_questions_info[i]['问题'] != score_map[score_type][i]['question']:
-                raise ValueError('question not match')
-            all_questions_info[i][score_type] = score_map[score_type][i][score_type]
-
-            if '问题类型' in all_questions_info[i]:
-                ques_type = all_questions_info[i]['问题类型'].strip()
-                ragas_score[ques_type][score_type].append(all_questions_info[i][score_type])  
-            ragas_score['all'][score_type].append(all_questions_info[i][score_type])
-        
-    df = pd.DataFrame(all_questions_info)
+    for metric, scores in score_map.items():
+        df[metric] = df.index.map({idx: rows[metric] for idx, rows in scores.iterrows()})
     df.to_excel(excel_file, index=False)
 
-    flat_score = []
-    for ques_type in ragas_score:
-        each_ques_type_score = dict()
-        each_ques_type_score['问题类型'] = ques_type
-        each_ques_type_score['问题个数'] = len(ragas_score[ques_type][list(score_map.keys())[0]])
-        for score_type in ragas_score[ques_type]:
-            avg_score = sum(ragas_score[ques_type][score_type]) / len(ragas_score[ques_type][score_type])
-            each_ques_type_score[score_type] = avg_score
-
-        flat_score.append(each_ques_type_score)
-    
-    return pd.DataFrame(flat_score)
+    if '问题类型' in df.columns:
+        grouped_df = (
+            df.groupby('问题类型')
+            .agg({'问题': 'count', **{metric: 'mean' for metric in score_map}})
+            .rename(columns={'问题': '问题数量'})
+        )
+        total_question = grouped_df['问题数量'].sum()
+        grouped_df.loc['all', '问题数量'] = total_question
+        for metric in score_map:
+            grouped_df.loc['all', metric] = df[metric].sum() / total_question
+        return grouped_df
 
 
 if __name__ == '__main__':
-    excel_file = '../data/questions_info_with_answer_sample_qwen14b_12chunk.xlsx'
+    excel_file = '/opt/bisheng/src/bisheng-langchain/experimental/rag/scoring/data/benchmark_v1.0.xlsx'
     print(rag_benchmark_scoring(excel_file))
