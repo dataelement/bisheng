83ac387e9f78ceddf86fbf6a0c5ffba08a58b770
gulixin0922 <gulixin@dataelem.com>
2024-02-02 14:22:57 +0800

    update travel assistant

---
+++ src/bisheng-langchain/experimental/travel_assistant/travel.py
@@ -0,0 +1,104 @@
+import os
+import httpx
+import gradio as gr
+import re
+from typing import Optional, Tuple
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from prompt import system_template
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+openai_api_key = os.environ.get('OPENAI_API_KEY', '')
+openai_proxy = os.environ.get('OPENAI_PROXY', '')
+
+
+def initial_chain(assistant_message):
+    travel_prompt = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(assistant_message),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{question}")
+        ]
+    )
+
+    llm = ChatOpenAI(model="gpt-4-0125-preview", 
+                    temperature=0.0,
+                    openai_api_key=openai_api_key,
+                    http_client=httpx.Client(proxies=openai_proxy))
+    # Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+    # Notice that `"chat_history"` aligns with the MessagesPlaceholder name.
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    global conversation
+    conversation = LLMChain(llm=llm, prompt=travel_prompt, verbose=True, memory=memory)
+    return [('正在初始化助手', '初始化助手成功请开始对话')]
+
+
+def clear_session(assistant_message):
+    initial_chain(assistant_message)
+    return [], []
+
+
+def response_parse(json_string: str) -> str:
+    match = re.search(r"```(json)?(.*)```", json_string, re.DOTALL)
+    if match is None:
+        json_str = ''
+    else:
+        json_str = match.group(2)
+
+    json_str = json_str.strip()
+    json_str = json_str.replace('```', '')
+
+    match = re.search(r"{.*}", json_str, re.DOTALL)
+    if match is None:
+        json_str = json_str
+    else:
+        json_str = match.group(0)
+
+    if json_str.endswith('}\n}'):
+        json_str = json_str[:-2]
+    if json_str.startswith('{\n{'):
+        json_str = json_str.replace('{\n{', '{', 1)
+
+    print(f'llm response after parse: {json_str}')
+    # todo：清掉历史memory
+    
+    return json_str
+
+
+def predict(command, history: Optional[Tuple[str, str]]):
+    history = history or []
+    response = conversation({"question": command})['text']
+    history.append((command, response))
+    return history, history, '', response_parse(response)
+
+
+if __name__ == "__main__":
+    title = """差旅智能体输入解析多轮引导"""
+    with gr.Blocks() as demo:
+        gr.Markdown(title)
+
+        assistant_message = gr.Textbox(label='自定义你的专属差旅助手', value=system_template, interactive=True, lines=2)
+
+        with gr.Row():
+            with gr.Column(scale=2):
+                chatbot = gr.Chatbot()
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", container=False)
+                with gr.Row():
+                    initialBtn = gr.Button("🙂initial assistant")
+                    submitBtn = gr.Button("🚀Submit", variant="primary")
+                    emptyBtn = gr.Button("🧹Clear History")
+            slot_show = gr.Textbox(label="最终用户提交信息", lines=20, interactive=False, scale=1)
+
+        state = gr.State([])
+
+        initialBtn.click(fn=initial_chain, inputs=[assistant_message], outputs=[chatbot])
+        submitBtn.click(fn=predict, inputs=[user_input, state], outputs=[chatbot, state, user_input, slot_show])
+        emptyBtn.click(fn=clear_session, inputs=[assistant_message], outputs=[chatbot, state])
+
+    demo.queue().launch(share=False, inbrowser=True, server_name="0.0.0.0", server_port=8000)
