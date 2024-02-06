c4dfc8e8de516cc48fffbf87f48c542d27ee0a51
GuoQing Zhang <zhangguoqing@dataelem.com>
2024-02-06 17:31:11 +0800

    feat: change all sft api url

--- src/backend/bisheng/api/services/sft_backend.py
+++ src/backend/bisheng/api/services/sft_backend.py
@@ -27,37 +27,42 @@ class SFTBackend:
         options 为指令options参数
         params 为指令的command参数参数
         """
-        url = '/v2.1/sft/job'
+        uri = '/v2.1/sft/job'
+        url = '/v2.1/models/sft_elem/infer'
         res = requests.post(f'{host}{url}',
-                            json={'uri': url, 'job_id': job_id, 'options': cls.CMD_OPTIONS, 'params': params})
+                            json={'uri': uri, 'job_id': job_id, 'options': cls.CMD_OPTIONS, 'params': params})
         return cls.handle_response(res)
 
     @classmethod
     def cancel_job(cls, host: str, job_id: str) -> (bool, str | Dict):
         """ 取消训练任务 """
-        url = '/v2.1/sft/job/cancel'
-        res = requests.post(f'{host}{url}', json={'uri': url, 'job_id': job_id})
+        uri = '/v2.1/sft/job/cancel'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(f'{host}{url}', json={'uri': uri, 'job_id': job_id})
         return cls.handle_response(res)
 
     @classmethod
     def delete_job(cls, host: str, job_id: str, model_name: str) -> (bool, str | Dict):
         """ 删除训练任务 """
-        url = '/v2.1/sft/job/delete'
-        res = requests.post(f'{host}{url}', json={'uri': url, 'job_id': job_id, 'model_name': model_name})
+        uri = '/v2.1/sft/job/delete'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(f'{host}{url}', json={'uri': uri, 'job_id': job_id, 'model_name': model_name})
         return cls.handle_response(res)
 
     @classmethod
     def publish_job(cls, host: str, job_id: str, model_name: str) -> (bool, str | Dict):
         """ 发布训练任务 从训练路径到处到正式路径"""
-        url = '/v2.1/sft/job/publish'
-        res = requests.post(url, json={'uri': url, 'job_id': job_id, 'model_name': model_name})
+        uri = '/v2.1/sft/job/publish'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(url, json={'uri': uri, 'job_id': job_id, 'model_name': model_name})
         return cls.handle_response(res)
 
     @classmethod
     def cancel_publish_job(cls, host: str, job_id: str, model_name: str) -> (bool, str | Dict):
         """ 下架训练任务已发布的模型 """
-        url = '/v2.1/sft/job/publish/cancel'
-        res = requests.post(f'{host}{url}', json={'uri': url, 'job_id': job_id, 'model_name': model_name})
+        uri = '/v2.1/sft/job/publish/cancel'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(f'{host}{url}', json={'uri': uri, 'job_id': job_id, 'model_name': model_name})
         return cls.handle_response(res)
 
     @classmethod
@@ -70,8 +75,9 @@ class SFTBackend:
             "reason": "失败原因"
          }
         """
-        url = '/v2.1/sft/job/status'
-        res = requests.post(f'{host}{url}', json={'uri': url, 'job_id': job_id})
+        uri = '/v2.1/sft/job/status'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(f'{host}{url}', json={'uri': uri, 'job_id': job_id})
         return cls.handle_response(res)
 
     @classmethod
@@ -84,8 +90,9 @@ class SFTBackend:
             "log_data": 参考bisheng-ft生产的训练日志文件内容
         }
         """
-        url = '/v2.1/sft/job/log'
-        res = requests.post(f'{host}{url}', json={'uri': url, 'job_id': job_id})
+        uri = '/v2.1/sft/job/log'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(f'{host}{url}', json={'uri': uri, 'job_id': job_id})
         return cls.handle_response(res)
 
     @classmethod
@@ -97,15 +104,17 @@ class SFTBackend:
             "report": {}
         }
         """
-        url = '/v2.1/sft/job/metrics'
-        res = requests.post(f'{host}{url}', json={'uri': url, 'job_id': job_id})
+        uri = '/v2.1/sft/job/metrics'
+        url = '/v2.1/models/sft_elem/infer'
+        res = requests.post(f'{host}{url}', json={'uri': uri, 'job_id': job_id})
         return cls.handle_response(res)
 
     @classmethod
     def change_model_name(cls, host, job_id: str, old_model_name: str, model_name: str) -> (bool, str):
         """ 修改模型名称 """
-        url = '/v2.1/sft/job/model_name'
+        uri = '/v2.1/sft/job/model_name'
+        url = '/v2.1/models/sft_elem/infer'
         res = requests.post(f'{host}{url}',
-                            json={'uri': url, 'job_id': job_id, 'old_model_name': old_model_name,
+                            json={'uri': uri, 'job_id': job_id, 'old_model_name': old_model_name,
                                   'model_name': model_name})
         return cls.handle_response(res)
