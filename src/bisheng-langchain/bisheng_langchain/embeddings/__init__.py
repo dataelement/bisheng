f011259c4d991ebd25de92316dcbc13248a98d4d
jingwangyaun <jingwangyuan@dataelem.com>
2024-02-27 11:03:56 +0800

    embeding

--- src/bisheng-langchain/bisheng_langchain/embeddings/__init__.py
+++ src/bisheng-langchain/bisheng_langchain/embeddings/__init__.py
@@ -1,8 +1,10 @@
 from .host_embedding import (BGEZhEmbedding, CustomHostEmbedding, GTEEmbedding, HostEmbeddings,
                              ME5Embedding)
 from .wenxin import WenxinEmbeddings
+from .huggingfacemultilingual import HuggingFaceMultilingualEmbeddings
+from .huggingfacegte import HuggingFaceGteEmbeddings
 
 __all__ = [
     'WenxinEmbeddings', 'ME5Embedding', 'BGEZhEmbedding', 'GTEEmbedding',
-    'HostEmbeddings', 'CustomHostEmbedding'
+    'HostEmbeddings', 'CustomHostEmbedding', 'HuggingFaceMultilingualEmbeddings', 'HuggingFaceGteEmbeddings'
 ]
