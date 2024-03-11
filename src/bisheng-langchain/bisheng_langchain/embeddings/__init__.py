d2407a007b7eddc4fc89d8b260b4e3eb841eb8c4
sunjun <18810256350@163.com>
2024-03-11 23:06:52 +0800

    hostembedding support jina

--- src/bisheng-langchain/bisheng_langchain/embeddings/__init__.py
+++ src/bisheng-langchain/bisheng_langchain/embeddings/__init__.py
@@ -1,8 +1,8 @@
 from .host_embedding import (BGEZhEmbedding, CustomHostEmbedding, GTEEmbedding, HostEmbeddings,
-                             ME5Embedding)
+                             ME5Embedding, JINAEmbedding)
 from .wenxin import WenxinEmbeddings
 
 __all__ = [
     'WenxinEmbeddings', 'ME5Embedding', 'BGEZhEmbedding', 'GTEEmbedding',
-    'HostEmbeddings', 'CustomHostEmbedding'
+    'HostEmbeddings', 'CustomHostEmbedding', 'JINAEmbedding'
 ]
