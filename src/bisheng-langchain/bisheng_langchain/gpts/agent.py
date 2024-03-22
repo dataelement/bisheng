3ace02e9d4869e93a23e3ba2961448f245358bd5
youjiachen <youmcgrady@126.com>
2024-03-22 11:47:53 +0000

    update agent

--- src/bisheng-langchain/bisheng_langchain/gpts/agent.py
+++ src/bisheng-langchain/bisheng_langchain/gpts/agent.py
@@ -5,42 +5,16 @@ from urllib.parse import urlparse
 from functools import lru_cache
 from enum import Enum
 from typing import Any, Mapping, Optional, Sequence, Union
+from langchain.tools import BaseTool
 
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models.openai import ChatOpenAI
 from langchain_core.messages import AnyMessage, HumanMessage
 from langchain_core.runnables import (
     ConfigurableField,
     RunnableBinding,
 )
 from bisheng_langchain.gpts.agent_types import get_openai_functions_agent_executor
-from bisheng_langchain.gpts.tools import (
-    TOOLS,
-    ActionServer,
-    Arxiv,
-    Connery,
-    DDGSearch,
-    PressReleases,
-    PubMed,
-    SecFilings,
-    Tavily,
-    TavilyAnswer,
-    Wikipedia,
-    YouSearch,
-)
-
-Tool = Union[
-    ActionServer,
-    Connery,
-    DDGSearch,
-    Arxiv,
-    YouSearch,
-    SecFilings,
-    PressReleases,
-    PubMed,
-    Wikipedia,
-    Tavily,
-    TavilyAnswer,
-]
+from bisheng_langchain.gpts.tools import arxiv_tool
 
 logger = logging.getLogger(__name__)
 
@@ -58,7 +32,7 @@ def get_openai_llm(model_name):
             http_client = httpx.AsyncClient(proxies=proxy_url)
         else:
             logger.warn("Invalid proxy URL provided. Proceeding without proxy.")
-    
+
     llm = ChatOpenAI(
         http_client=http_client,
         model=model_name,
@@ -79,7 +53,7 @@ def get_agent_executor(
 
 
 class ConfigurableAgent(RunnableBinding):
-    tools: Sequence[Tool]
+    tools: Sequence[BaseTool]
     model_name: str
     system_message: str = DEFAULT_SYSTEM_MESSAGE
     interrupt_before_action: bool = False
@@ -87,7 +61,7 @@ class ConfigurableAgent(RunnableBinding):
     def __init__(
         self,
         *,
-        tools: Sequence[Tool],
+        tools: Sequence[BaseTool],
         model_name: str = "gpt-4-1106-preview",
         system_message: str = DEFAULT_SYSTEM_MESSAGE,
         interrupt_before_action: bool = False,
@@ -96,16 +70,11 @@ class ConfigurableAgent(RunnableBinding):
         **others: Any,
     ) -> None:
         others.pop("bound", None)
-        _tools = []
-        for _tool in tools:
-            tool_config = _tool.config if _tool.config else {}
-            _returned_tools = TOOLS[_tool.type](**tool_config)
-            if isinstance(_returned_tools, list):
-                _tools.extend(_returned_tools)
-            else:
-                _tools.append(_returned_tools)
         _agent = get_agent_executor(
-            _tools, model_name, system_message, interrupt_before_action
+            tools,
+            model_name,
+            system_message,
+            interrupt_before_action,
         )
         agent_executor = _agent.with_config({"recursion_limit": 50})
         super().__init__(
@@ -120,9 +89,11 @@ class ConfigurableAgent(RunnableBinding):
 
 if __name__ == "__main__":
     import asyncio
-    arxiv = Arxiv()
-    agent = ConfigurableAgent(model_name="gpt-4-1106-preview", tools=[arxiv], system_message=DEFAULT_SYSTEM_MESSAGE)
 
-    inputs = [HumanMessage(content="帮我查找一下关于人工智能的文章。")]
+    agent = ConfigurableAgent(
+        model_name="gpt-4-1106-preview", tools=[arxiv_tool], system_message=DEFAULT_SYSTEM_MESSAGE
+    )
+
+    inputs = [HumanMessage(content="帮我找一篇关于BERT的文章。")]
     result = asyncio.run(agent.ainvoke(inputs))
     print(result)
