69d6a7b3791e0a584fcce6ac114522593aa0e1d3
“kingofbadman” <1480934007a@gmail.com>
2024-03-29 18:51:20 +0800

    feat:修改了系统分栏的样式

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 const apiRoutes = ["^/api/", "/health"];
 import path from "path";
 // Use environment variable to determine the target.
-const target = process.env.VITE_PROXY_TARGET || "http://127.0.0.1:7860";
+const target = process.env.VITE_PROXY_TARGET || "http://192.168.106.120:3002";
 
 const proxyTargets = apiRoutes.reduce((proxyObj, route) => {
   proxyObj[route] = {
