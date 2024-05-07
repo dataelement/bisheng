3fc525e6446e5f03875dc13f7598eec3b68603e9
gulixin0922 <gulixin@dataelem.com>
2024-05-07 09:04:51 +0000

    update bisheng-langchain rag

---
+++ src/bisheng-langchain/bisheng_langchain/rag/prompts/prompt.py
@@ -0,0 +1,47 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+
+
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{context}
+
+Question: {question}
+Helpful Answer:"""
+BASE_PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
+system_template = """Use the following pieces of context to answer the user's question. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----------------
+{context}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+CHAT_PROMPT = ChatPromptTemplate.from_messages(messages)
+
+
+system_template_general = """你是一个准确且可靠的知识库问答助手，能够借助上下文知识回答问题。你需要根据以下的规则来回答问题：
+1. 如果上下文中包含了正确答案，你需要根据上下文进行准确的回答。但是在回答前，你需要注意，上下文中的信息可能也包含存在事实性错误，如果文档中存在和事实不一致的错误，请根据事实回答。
+2. 如果上下文中不包含答案，就说你不知道，不要试图编造答案。
+3. 你需要根据上下文给出详细的回答，不要试图偷懒，不要遗漏括号中的信息，你必须回答的尽可能详细。
+"""
+human_template_general = """
+上下文：
+{context}
+
+问题：
+{question}
+"""
+messages_general = [
+    SystemMessagePromptTemplate.from_template(system_template_general),
+    HumanMessagePromptTemplate.from_template(human_template_general),
+]
+CHAT_PROMPT_GENERAL = ChatPromptTemplate.from_messages(messages_general)
---
+++ src/bisheng-langchain/bisheng_langchain/rag/scoring/ragas_score.py
@@ -0,0 +1,183 @@
+import json
+import os
+import re
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import pandas as pd
+from datasets import Dataset
+from loguru import logger
+from ragas import evaluate
+from ragas.metrics import AnswerCorrectness, AnswerCorrectnessBisheng, AnswerRecallBisheng
+
+
+@dataclass
+class RagScore:
+    excel_path: str
+    save_path: str
+    question_column: str
+    gt_column: str
+    answer_column: str
+    metrics: List[str]
+    contexts_column: Optional[str] = None
+    query_type_column: Optional[str] = None
+    gt_split_column: Optional[str] = None
+    batch_size: int = 5
+    whether_gtsplit: bool = False
+
+    def _validate_metrics(self):
+        for metric in self.metrics:
+            if not hasattr(self, f'ragas_{metric}'):
+                raise Exception(f'"ragas_{metric}" 未实现!')
+
+    def ragas_answer_correctness(self, dataset: Dataset) -> pd.DataFrame:
+        # answer_correctness, 只考虑事实相似度
+        weights = [1.0, 0.0]
+        answer_correctness = AnswerCorrectness(weights=weights, batch_size=self.batch_size)
+        result = evaluate(
+            dataset=dataset,
+            metrics=[
+                answer_correctness,
+            ],
+        )
+        self.score_map_keys = list(result.keys())
+        df = result.to_pandas()
+        return df
+
+    def ragas_answer_correctness_bisheng(self, dataset: Dataset) -> pd.DataFrame:
+        answer_correctness = AnswerCorrectnessBisheng(batch_size=self.batch_size)
+        result = evaluate(
+            dataset=dataset,
+            metrics=[answer_correctness],
+        )
+        self.score_map_keys = list(result.keys())
+        df = result.to_pandas()
+        return df
+
+    def ragas_answer_recall_bisheng(self, dataset: Dataset) -> pd.DataFrame:
+        answer_recall =AnswerRecallBisheng(batch_size=self.batch_size, 
+                                           whether_gtsplit=self.whether_gtsplit)
+        result = evaluate(
+            dataset=dataset,
+            metrics=[answer_recall],
+        )
+        self.score_map_keys = list(result.keys())
+        df = result.to_pandas()
+        return df
+
+    def _remove_source(self, pred: str) -> str:
+        """去除【1†source】, only for openai assistant"""
+        pattern = re.compile("【(\d+)†source】")
+        match = re.findall(pattern, pred)
+        for i in match:
+            str_temp = f"【{i}†source】"
+            pred = pred.replace(str_temp, '')
+        return pred
+
+    def score(self) -> Tuple[pd.DataFrame, Optional[pd.DataFrame]]:
+        df = pd.read_excel(self.excel_path)
+        ori_row_nums = df.shape[0]
+
+        # 删除含有na的行
+        columns_to_check = [
+            self.question_column,
+            self.gt_column,
+            self.answer_column,
+            self.contexts_column,
+            self.query_type_column,
+        ]
+        # 是否有要点拆分列
+        if self.gt_split_column:
+            columns_to_check.append(self.gt_split_column)
+
+        df.dropna(subset=[col for col in columns_to_check if col], inplace=True)
+        df = df.reset_index()
+        print(f'删除含有na的行 {ori_row_nums - df.shape[0]} 个!')
+        print(f'总计 {df.shape[0]} 个问题')
+
+        questions = df[self.question_column].tolist()
+        answers = df[self.answer_column].tolist()
+        # answers = df[self.answer_column].apply(self._remove_source).tolist() # for openai assistant
+        ground_truths = df[self.gt_column].apply(lambda x: [x]).tolist()
+        # todo: contexts可能是保存在json中的，这段代码可能需要修改
+        contexts = (
+            [['']] * len(questions)
+            if not self.contexts_column
+            else df[self.contexts_column].apply(lambda x: [x]).tolist()
+        )
+        # To dict
+        if self.gt_split_column:
+            gtsplit = df[self.gt_split_column].tolist()
+            data: Dict[str, List[Any]] = {
+                "question": questions,
+                "answer": answers,
+                "contexts": contexts,
+                "ground_truths": ground_truths,
+                'gt_split_point': gtsplit
+            }
+        else:
+            data: Dict[str, List[Any]] = {
+                "question": questions,
+                "answer": answers,
+                "contexts": contexts,
+                "ground_truths": ground_truths,
+            }
+        # Convert dict to dataset
+        dataset = Dataset.from_dict(data)
+
+        self._validate_metrics()
+
+        save_group_df = dict()
+        for metric_name in self.metrics:
+            ragas_result = getattr(self, f'ragas_{metric_name}')(dataset)
+            if metric_name =='answer_recall_bisheng':
+                if self.gt_split_column:
+                    df[self.gt_split_column] = ragas_result["gt_split_point"]
+                else:
+                    df["gt_split_point"] = ragas_result["gt_split_point"]
+                df["analyse"] = ragas_result["analyse"]
+
+            score_map = dict().fromkeys(self.score_map_keys, ragas_result)
+            for metric, scores in score_map.items():
+                df[metric] = df.index.map({idx: rows[metric] for idx, rows in scores.iterrows()})
+
+            if self.query_type_column and self.query_type_column in df.columns:
+                grouped_df = df.groupby(self.query_type_column)
+                grouped_df = grouped_df.agg({self.question_column: 'count', **{metric: 'mean' for metric in score_map}})
+                grouped_df.rename(columns={self.question_column: '问题个数'}, inplace=True)
+                
+                total_question = grouped_df['问题个数'].sum()
+                grouped_df.loc['总计', '问题个数'] = total_question
+                for metric in score_map:
+                    grouped_df.loc['总计', metric] = df[metric].sum() / total_question
+                save_group_df[f'{metric_name}_group'] = grouped_df
+                
+                print(grouped_df.to_markdown())
+
+        # save
+        output_path = Path(self.save_path) / f"{Path(self.excel_path).stem}_score.xlsx"
+        with pd.ExcelWriter(output_path) as writer:
+            df.to_excel(writer, sheet_name='Sheet1', index=False)
+            if len(save_group_df):
+                for metric, grouped_df in save_group_df.items():
+                    grouped_df.to_excel(writer, sheet_name=metric, index=True)
+        print(f'保存到 {output_path} 成功!')
+
+
+if __name__ == '__main__':
+    params = {
+        'excel_path': '/home/gulixin/workspace/llm/bisheng/src/bisheng-langchain/experimental/rag/data/test.xlsx',
+        'save_path': '/home/gulixin/workspace/llm/bisheng/src/bisheng-langchain/experimental/rag/data',
+        'question_column': '问题',
+        'gt_column': 'GT',
+        'answer_column': 'rag_answer',
+        'query_type_column': '问题类型',
+        # 'metrics': ['answer_correctness_bisheng'],
+        'metrics': ['answer_recall_bisheng'],
+        'batch_size': 10,
+        'whether_gtsplit': False,
+    }
+    rag_score = RagScore(**params)
+    rag_score.score()
