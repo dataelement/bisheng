aedd07f814b12e4148f1f5d81653d1d0a3fe00bf
gulixin0922 <gulixin@dataelem.com>
2024-06-03 16:21:08 +0800

    add qa prompt

--- src/bisheng-langchain/bisheng_langchain/rag/bisheng_rag_chain.py
+++ src/bisheng-langchain/bisheng_langchain/rag/bisheng_rag_chain.py
@@ -10,14 +10,43 @@ from langchain_core.callbacks import (
     CallbackManagerForChainRun,
     Callbacks
 )
-from langchain_core.documents import Document
+from langchain_core.prompts import PromptTemplate, BasePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
 from langchain_core.language_models import BaseLanguageModel
-from langchain_core.prompts import PromptTemplate
 from langchain_core.pydantic_v1 import Extra, Field
 from bisheng_langchain.vectorstores import ElasticKeywordsSearch, Milvus
 
 from langchain.chains.base import Chain
 from .bisheng_rag_tool import BishengRAGTool
+
+
+# system_template = """Use the following pieces of context to answer the user's question. 
+# If you don't know the answer, just say that you don't know, don't try to make up an answer.
+# ----------------
+# {context}"""
+# messages = [
+#     SystemMessagePromptTemplate.from_template(system_template),
+#     HumanMessagePromptTemplate.from_template("{question}"),
+# ]
+# DEFAULT_QA_PROMPT = ChatPromptTemplate.from_messages(messages)
+
+
+system_template_general = """你是一个准确且可靠的知识库问答助手，能够借助上下文知识回答问题。你需要根据以下的规则来回答问题：
+1. 如果上下文中包含了正确答案，你需要根据上下文进行准确的回答。但是在回答前，你需要注意，上下文中的信息可能存在事实性错误，如果文档中存在和事实不一致的错误，请根据事实回答。
+2. 如果上下文中不包含答案，就说你不知道，不要试图编造答案。
+3. 你需要根据上下文给出详细的回答，不要试图偷懒，不要遗漏括号中的信息，你必须回答的尽可能详细。
+"""
+human_template_general = """
+上下文：
+{context}
+
+问题：
+{question}
+"""
+messages_general = [
+    SystemMessagePromptTemplate.from_template(system_template_general),
+    HumanMessagePromptTemplate.from_template(human_template_general),
+]
+DEFAULT_QA_PROMPT = ChatPromptTemplate.from_messages(messages_general)
 
 
 class BishengRetrievalQA(Chain):
@@ -64,6 +93,7 @@ class BishengRetrievalQA(Chain):
         llm: BaseLanguageModel,
         vector_store: Milvus,
         keyword_store: ElasticKeywordsSearch,
+        QA_PROMPT: ChatPromptTemplate = DEFAULT_QA_PROMPT,
         max_content: int = 15000,
         sort_by_source_and_index: bool = True,
         callbacks: Callbacks = None,
@@ -73,6 +103,7 @@ class BishengRetrievalQA(Chain):
             vector_store=vector_store, 
             keyword_store=keyword_store,
             llm=llm,
+            QA_PROMPT=QA_PROMPT,
             max_content=max_content,
             sort_by_source_and_index=sort_by_source_and_index,
             **kwargs
