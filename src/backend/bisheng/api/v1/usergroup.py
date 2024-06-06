5df90202a93a8e2b99ae0312b2c56bad5224788c
yaojin <yaojin@dataelem.com>
2024-06-06 22:19:45 +0800

    feat: group resource

--- src/backend/bisheng/api/v1/usergroup.py
+++ src/backend/bisheng/api/v1/usergroup.py
@@ -5,6 +5,7 @@ from bisheng.api.services.role_group_service import RoleGroupService
 from bisheng.api.utils import check_permissions
 from bisheng.api.v1.schemas import UnifiedResponseModel
 from bisheng.database.models.group import GroupRead
+from bisheng.database.models.group_resource import ResourceTypeEnum
 from bisheng.database.models.user_group import UserGroupCreate, UserGroupRead
 from fastapi import APIRouter, Body, Depends
 from fastapi_jwt_auth import AuthJWT
@@ -51,10 +52,14 @@ async def set_group_admin(user_ids: Annotated[List[int], Body(embed=True)],
 
 
 @router.get('/get_group_flows', response_model=UnifiedResponseModel(List[UserGroupRead]))
-async def get_group_flows(user_group: UserGroupCreate, Authorize: AuthJWT = Depends()):
+async def get_group_flows(*,
+                          group_id: Annotated[int, Body(embed=True)],
+                          resource_type: Annotated[int, Body(embed=True)],
+                          Authorize: AuthJWT = Depends()):
     """
     获取所有分组
     """
     # await check_permissions(Authorize, ['admin'])
 
-    return RoleGroupService().get_group_list()
+    return RoleGroupService().get_group_resources(group_id,
+                                                  resource_type=ResourceTypeEnum(resource_type))
