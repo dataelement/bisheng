76681cfc7ded8e818524e04f02388833fe1c038b
mapan <mapan@lixiang.com>
2024-06-07 01:16:03 +0800

    feat: get evaluation list

--- src/backend/bisheng/api/services/evaluation.py
+++ src/backend/bisheng/api/services/evaluation.py
@@ -1,50 +1,78 @@
-from datetime import datetime
-from typing import Any, List, Optional
-from uuid import UUID
+import os
+import uuid
+from typing import List
 
-from bisheng.api.errcode.assistant import (AssistantInitError, AssistantNameRepeatError,
-                                           AssistantNotEditError, AssistantNotExistsError, ToolTypeRepeatError,
-                                           ToolTypeEmptyError, ToolTypeNotExistsError, ToolTypeIsPresetError)
-from bisheng.api.errcode.base import UnAuthorizedError
-from bisheng.api.services.assistant_agent import AssistantAgent
-from bisheng.api.services.assistant_base import AssistantUtils
+from fastapi import UploadFile
+
 from bisheng.api.services.user_service import UserPayload
-from bisheng.api.v1.schemas import (AssistantInfo, AssistantSimpleInfo, AssistantUpdateReq,
-                                    StreamData, UnifiedResponseModel, resp_200, resp_500)
+from bisheng.api.v1.schemas import (UnifiedResponseModel, resp_200)
 from bisheng.cache import InMemoryCache
-from bisheng.database.models.assistant import (Assistant, AssistantDao, AssistantLinkDao,
-                                               AssistantStatus)
-from bisheng.database.models.evaluation import (Evaluation, EvaluationDao)
-from bisheng.database.models.flow import Flow, FlowDao
-from bisheng.database.models.gpts_tools import GptsToolsDao, GptsToolsRead, GptsToolsTypeRead, GptsTools
-from bisheng.database.models.knowledge import KnowledgeDao
-from bisheng.database.models.role_access import AccessType, RoleAccessDao
+from bisheng.database.models.flow import FlowDao
+from bisheng.database.models.flow_version import FlowVersionDao
+from bisheng.database.models.assistant import AssistantDao
+from bisheng.database.models.evaluation import (Evaluation, EvaluationDao, ExecType)
 from bisheng.database.models.user import UserDao
-from bisheng.database.models.user_role import UserRoleDao
-from loguru import logger
+from bisheng.utils.minio_client import MinioClient
+from fastapi.encoders import jsonable_encoder
 
 
-class EvaluationService():
+class EvaluationService:
     UserCache: InMemoryCache = InMemoryCache()
 
     @classmethod
-    def get_assistant(cls,
-                      user: UserPayload,
-                      status: int | None = None,
-                      page: int = 1,
-                      limit: int = 20) -> UnifiedResponseModel[List[Evaluation]]:
+    def get_evaluation(cls,
+                       user: UserPayload,
+                       page: int = 1,
+                       limit: int = 20) -> UnifiedResponseModel[List[Evaluation]]:
         """
         获取测评任务列表
         """
         data = []
-        res, total = EvaluationDao.get_all_evaluations(page, limit)
+        res_evaluations, total = EvaluationDao.get_my_evaluations(user.user_id, page, limit)
 
-        for one in res:
-            simple_dict = one.model_dump(include={
-                'id', 'exec_type', 'unique_id', 'status', 'user_id', 'create_time', 'update_time'
-            })
-            simple_dict['user_name'] = cls.get_user_name(one.user_id)
-            data.append(Evaluation(**simple_dict))
+        # 技能ID列表
+        flow_ids = []
+        # 助手ID列表
+        assistant_ids = []
+        # 版本ID列表
+        flow_version_ids = []
+
+        for one in res_evaluations:
+            if one.exec_type == ExecType.FLOW.value:
+                flow_ids.append(one.unique_id)
+                if one.version:
+                    flow_version_ids.append(one.version)
+            if one.exec_type == ExecType.ASSISTANT.value:
+                assistant_ids.append(one.unique_id)
+
+        flow_names = {}
+        flow_versions = {}
+        assistant_names = {}
+
+        if flow_ids:
+            flows = FlowDao.get_flow_by_ids(flow_ids=flow_ids)
+            flow_names = {str(one.id): one.name for one in flows}
+
+        if flow_version_ids:
+            versions = FlowVersionDao.get_list_by_ids(ids=flow_version_ids)
+            flow_versions = {one.id: one.name for one in versions}
+
+        if assistant_ids:
+            assistants = AssistantDao.get_assistants_by_ids(assistant_ids=assistant_ids)
+            assistant_names = {str(one.id): one.name for one in assistants}
+
+        for one in res_evaluations:
+            evaluation_item = jsonable_encoder(one)
+            if one.exec_type == ExecType.FLOW.value:
+                evaluation_item['unique_name'] = flow_names.get(one.unique_id)
+            if one.exec_type == ExecType.ASSISTANT.value:
+                evaluation_item['unique_name'] = assistant_names.get(one.unique_id)
+            if one.version:
+                evaluation_item['version_name'] = flow_versions.get(one.version)
+
+            evaluation_item['user_name'] = cls.get_user_name(one.user_id)
+            data.append(evaluation_item)
+
         return resp_200(data={'data': data, 'total': total})
 
     @classmethod
@@ -59,3 +87,18 @@ class EvaluationService():
             return f'{user_id}'
         cls.UserCache.set(user_id, user)
         return user.user_name
+
+    @classmethod
+    def upload_file(cls, file: UploadFile):
+        minio_client = MinioClient()
+        file_id = uuid.uuid4().hex
+        file_name = file.filename
+
+        file_ext = os.path.basename(file.filename).split('.')[-1]
+        file_path = f'{EvaluationService.get_file_root()}/{file_id}.{file_ext}'
+        minio_client.upload_minio_file(file_path, file.file, file.size, content_type=file.content_type)
+        return file_name, file_path
+
+    @classmethod
+    def get_file_root(cls):
+        return "evaluation/dataset"
--- src/backend/bisheng/api/v1/evaluation.py
+++ src/backend/bisheng/api/v1/evaluation.py
@@ -1,56 +1,27 @@
 import json
-import os
-import re
-import time
 from typing import List, Optional
-from uuid import uuid4
-
-from bisheng.api.services.knowledge_imp import (addEmbedding, decide_vectorstores,
-                                                delete_knowledge_file_vectors, retry_files)
-from bisheng.api.utils import access_check
-from bisheng.api.v1.schemas import UnifiedResponseModel, UploadFileResponse, resp_200, resp_500
-from bisheng.cache.utils import file_download, save_uploaded_file
+from bisheng.api.v1.schemas import UnifiedResponseModel, resp_200
 from bisheng.database.base import session_getter
 from bisheng.api.services.evaluation import EvaluationService
 from bisheng.api.services.user_service import UserPayload
-from bisheng.database.models.knowledge import (Knowledge, KnowledgeCreate, KnowledgeDao,
-                                               KnowledgeRead)
-from bisheng.database.models.knowledge_file import (KnowledgeFile, KnowledgeFileDao,
-                                                    KnowledgeFileRead)
-from bisheng.database.models.role_access import AccessType, RoleAccess
-from bisheng.database.models.user import User
-from bisheng.interface.embeddings.custom import FakeEmbedding
-from bisheng.settings import settings
-from bisheng.utils.logger import logger
-from bisheng.utils.minio_client import MinioClient
-from bisheng_langchain.vectorstores import ElasticKeywordsSearch
-from fastapi import APIRouter, BackgroundTasks, Depends, File, HTTPException, UploadFile, Form
-from fastapi.encoders import jsonable_encoder
+from fastapi import Form
 from fastapi_jwt_auth import AuthJWT
-from fastapi import APIRouter, Body, Depends, HTTPException, Query, WebSocket, WebSocketException, UploadFile, File
+from fastapi import APIRouter, Depends, Query, UploadFile
 from bisheng.database.models.evaluation import EvaluationRead, EvaluationCreate, Evaluation
-from langchain_community.document_loaders import (BSHTMLLoader, PyPDFLoader, TextLoader,
-                                                  UnstructuredMarkdownLoader,
-                                                  UnstructuredPowerPointLoader,
-                                                  UnstructuredWordDocumentLoader)
-from pymilvus import Collection
-from sqlalchemy import delete, func, or_
-from sqlmodel import select
 
 router = APIRouter(prefix='/evaluation', tags=['Skills'])
 
 
 @router.get('', response_model=UnifiedResponseModel[List[Evaluation]])
-def get_assistant(*,
-                  name: str = Query(default=None, description='助手名称，模糊匹配, 包含描述的模糊匹配'),
-                  page: Optional[int] = Query(default=1, gt=0, description='页码'),
-                  limit: Optional[int] = Query(default=10, gt=0, description='每页条数'),
-                  status: Optional[int] = Query(default=None, description='是否上线状态'),
-                  Authorize: AuthJWT = Depends()):
+def get_evaluation(*,
+                   page: Optional[int] = Query(default=1, gt=0, description='页码'),
+                   limit: Optional[int] = Query(default=10, gt=0, description='每页条数'),
+                   Authorize: AuthJWT = Depends()):
+    """ 获取评测任务列表. """
     Authorize.jwt_required()
     current_user = json.loads(Authorize.get_jwt_subject())
     user = UserPayload(**current_user)
-    return EvaluationService.get_assistant(user, status, page, limit)
+    return EvaluationService.get_evaluation(user, page, limit)
 
 
 @router.post('/create', response_model=UnifiedResponseModel[EvaluationRead], status_code=201)
@@ -66,13 +37,14 @@ def create_evaluation(*,
     payload = json.loads(authorize.get_jwt_subject())
     user_id = payload.get('user_id')
 
-    db_evaluation = Evaluation.model_validate(
-        EvaluationCreate(unique_id=unique_id,
-                         exec_type=exec_type,
-                         version=version,
-                         prompt=prompt,
-                         user_id=user_id))
-
+    file_name, file_path = EvaluationService.upload_file(file=file)
+    db_evaluation = Evaluation.model_validate(EvaluationCreate(unique_id=unique_id,
+                                                               exec_type=exec_type,
+                                                               version=version,
+                                                               prompt=prompt,
+                                                               user_id=user_id,
+                                                               file_name=file_name,
+                                                               file_path=file_path))
     with session_getter() as session:
         session.add(db_evaluation)
         session.commit()
