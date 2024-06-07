1813eb616c9f6fb5daa69394cb00e2ff2cc7f483
yaojin <yaojin@dataelem.com>
2024-06-07 17:58:16 +0800

    feat: user group

--- src/backend/bisheng/database/models/group.py
+++ src/backend/bisheng/database/models/group.py
@@ -1,6 +1,5 @@
-from ast import Dict
 from datetime import datetime
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from bisheng.database.base import session_getter
 from bisheng.database.models.base import SQLModelSerializable
