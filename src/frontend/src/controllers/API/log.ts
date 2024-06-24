f11d123db65108a6384c0a4b3a52d29176511b35
MyGit <3282299728@qq.com>
2024-06-24 11:28:00 +0800

    style:api参数调整

--- src/frontend/src/controllers/API/log.ts
+++ src/frontend/src/controllers/API/log.ts
@@ -1,7 +1,7 @@
 import axios from "../request";
 
 // 分页获取审计列表
-export async function getLogsApi({page, pageSize, userIds, groupId, start, end, moduleId, action}:{
+export async function getLogsApi({page, pageSize, userIds, groupId = '', start, end, moduleId = '', action = ''}:{
     page:number,
     pageSize:number,
     userIds?:number[],
@@ -15,19 +15,20 @@ export async function getLogsApi({page, pageSize, userIds, groupId, start, end, 
     const startStr = start ? `&start_time=${start}` : ''
     const endStr = end ? `&end_time=${end}` : ''
     return await axios.get(
-        `/api/v1/audit?page=${page}&limit=${pageSize}&group_ids=${groupId || ''}${uids}` + 
-        `&system_id=${moduleId || ''}&event_type=${action || ''}` + startStr + endStr
+        `/api/v1/audit?page=${page}&limit=${pageSize}&group_ids=${groupId}${uids}` + 
+        `&system_id=${moduleId}&event_type=${action}` + startStr + endStr
     )
 }
+
 // 系统模块
 export async function getModulesApi():Promise<{data:any[]}> {
     return {
         data: [{name:'会话', value:'chat'},{name:'构建', value:'build'},{name:'知识库', value:'knowledge'},{name:'系统', value:'system'}]
     }
 }
+
 // 全部操作行为
 export async function getActionsApi():Promise<{data:any[]}> {
-    console.log('-----------')
     return {
         data: [
             {name:'新建会话',value:'create_chat'},{name:'删除会话',value:'delete_chat'},{name:'新建应用',value:'create_build'},{name:'编辑应用',value:'update_build'},
@@ -38,6 +39,7 @@ export async function getActionsApi():Promise<{data:any[]}> {
         ]
     }
 }
+
 // 系统模块下操作行为
 export async function getActionsByModuleApi(moduleId) {
     return {
