5a59ace620ebbcdb088c0c92b890c54c80a9b40a
dolphin <shanghang@dataelem.com>
2024-10-21 16:38:01 +0800

    feat: 标注人列表更新接口

--- src/frontend/src/components/bs-comp/tableComponent/ColFilterUser.tsx
+++ src/frontend/src/components/bs-comp/tableComponent/ColFilterUser.tsx
@@ -5,16 +5,16 @@ import { SearchInput } from "@/components/bs-ui/input";
 import { Label } from "@/components/bs-ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/bs-ui/popover";
 import { useDebounce } from "@/components/bs-ui/utils";
-import { getUsersApi } from "@/controllers/API/user";
+import { getLabelUsersApi, getUsersApi } from "@/controllers/API/user";
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-export default function ColFilterUser({ onFilter }) {
+export default function ColFilterUser({ label, onFilter }) {
     const { t } = useTranslation()
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState([]);
 
-    const { options, setOptions, reload, loadMore, search } = useUsersOptions()
+    const { options, setOptions, reload, loadMore, search } = useUsersOptions(label)
     const searchDb = useDebounce(search, 200, false)
 
     const handlerChecked = (id) => {
@@ -35,7 +35,7 @@ export default function ColFilterUser({ onFilter }) {
         </PopoverTrigger>
         <PopoverContent>
             <div>
-                <SearchInput placeholder={'用户名称'} className="w-[240px]" onChange={(e) => searchDb(e.target.value)}></SearchInput>
+                {!label && <SearchInput placeholder={'用户名称'} className="w-[240px]" onChange={(e) => searchDb(e.target.value)}></SearchInput>}
                 <div className="mt-2 max-h-[260px] min-h-20 overflow-y-auto">
                     {options.map((el) => (
                         <div className="flex items-center space-x-2 mb-1" key={el.value}>
@@ -65,16 +65,17 @@ export default function ColFilterUser({ onFilter }) {
 };
 
 // users
-const useUsersOptions = () => {
+const useUsersOptions = (label) => {
     const [options, setOptions] = useState([])
     const optionsRef = useRef([])
     const pageRef = useRef(1)
     const keywordRef = useRef("")
 
     const loadApps = () => {
-        const page = pageRef.current
-        getUsersApi({ page, pageSize: 400, name: keywordRef.current }).then((res: any) => {
-            const newOptions = res.data.map(el => ({
+        const page = pageRef.current;
+        (label ? getLabelUsersApi(label) : getUsersApi({ page, pageSize: 400, name: keywordRef.current })).then((res: any) => {
+            const resData = label ? res : res.data
+            const newOptions = resData.map(el => ({
                 label: el.user_name,
                 value: el.user_id
             }))
