09b3abf7ff4c452834d50825d38b2b2d971ef2c0
刘志硕 <liuzhishuo@naixuejiaoyu.com>
2024-10-24 14:55:36 +0800

    fix: api
    fix api not found

--- src/backend/bisheng/api/v1/knowledge.py
+++ src/backend/bisheng/api/v1/knowledge.py
@@ -295,7 +295,7 @@ def qa_add(*, QACreate: QAKnowledgeUpsert, login_user: UserPayload = Depends(get
     if db_knowledge.type != KnowledgeTypeEnum.QA.value:
         raise HTTPException(status_code=404, detail='知识库类型错误')
 
-    db_q = QAKnoweldgeDao.get_qa_knowledge_by_name(QACreate.questions)
+    db_q = QAKnoweldgeDao.get_qa_knowledge_by_name(QACreate.questions, QACreate.knowledge_id)
     if db_q:
         return resp_500(data=":该问题已被标注过")
 
