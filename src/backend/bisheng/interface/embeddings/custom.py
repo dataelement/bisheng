eb4113bbf7c509fc85923937a49bd31b79c9c925
yaojin <yaojin@dataelem.com>
2024-11-22 16:57:52 +0800

    add qa retriever

--- src/backend/bisheng/interface/embeddings/custom.py
+++ src/backend/bisheng/interface/embeddings/custom.py
@@ -1,15 +1,13 @@
 from typing import List, Optional
 
+from bisheng.database.models.llm_server import (LLMDao, LLMModel, LLMModelType, LLMServer,
+                                                LLMServerType)
+from bisheng.interface.importing import import_by_type
+from bisheng.interface.utils import wrapper_bisheng_model_limit_check
+from langchain.embeddings.base import Embeddings
+from langchain_core.pydantic_v1 import BaseModel
 from loguru import logger
 from pydantic import Field
-
-from langchain_core.pydantic_v1 import BaseModel
-from langchain.embeddings.base import Embeddings
-
-from bisheng.database.models.llm_server import LLMServerType, LLMDao, LLMModelType, LLMServer, LLMModel
-from bisheng.interface.importing import import_by_type
-
-from bisheng.interface.utils import wrapper_bisheng_model_limit_check
 
 
 class OpenAIProxyEmbedding(Embeddings):
@@ -50,12 +48,12 @@ class FakeEmbedding(Embeddings):
 class BishengEmbedding(BaseModel, Embeddings):
     """ Use the embedding model that has been launched in model management """
 
-    model_id: int = Field(default=0, description="后端服务保存的model唯一ID")
-    model: str = Field(default="", description="后端服务保存的model名称")
-    embedding_ctx_length: int = Field(default=8192, description="embedding模型上下文长度")
-    max_retries: int = Field(default=6, description="embedding模型调用失败重试次数")
-    request_timeout: int = Field(default=200, description="embedding模型调用超时时间")
-    model_kwargs: dict = Field(default={}, description="embedding模型调用参数")
+    model_id: int = Field(default=0, description='后端服务保存的model唯一ID')
+    model: str = Field(default='', description='后端服务保存的model名称')
+    embedding_ctx_length: int = Field(default=8192, description='embedding模型上下文长度')
+    max_retries: int = Field(default=6, description='embedding模型调用失败重试次数')
+    request_timeout: int = Field(default=200, description='embedding模型调用超时时间')
+    model_kwargs: dict = Field(default={}, description='embedding模型调用参数')
 
     embeddings: Optional[Embeddings] = Field(default=None)
     llm_node_type = {
@@ -103,7 +101,8 @@ class BishengEmbedding(BaseModel, Embeddings):
             raise Exception(f'只支持Embedding类型的模型，不支持{model_info.model_type}类型的模型')
         if not ignore_online and not model_info.online:
             raise Exception(f'{server_info.name}下的{model_info.model_name}模型已下线，请联系管理员上线对应的模型')
-        logger.debug(f'init_bisheng_embedding: server_id: {server_info.id}, model_id: {model_info.id}')
+        logger.debug(
+            f'init_bisheng_embedding: server_id: {server_info.id}, model_id: {model_info.id}')
         self.model_info: LLMModel = model_info
         self.server_info: LLMServer = server_info
         self.model = model_info.model_name
@@ -111,6 +110,8 @@ class BishengEmbedding(BaseModel, Embeddings):
         class_object = self._get_embedding_class(server_info.type)
         params = self._get_embedding_params(server_info, model_info)
         try:
+            if server_info.type == LLMServerType.OLLAMA.value:
+                params['query_instruction'] = 'passage: '
             self.embeddings = instantiate_embedding(class_object, params)
         except Exception as e:
             logger.exception('init_bisheng_embedding error')
@@ -134,18 +135,20 @@ class BishengEmbedding(BaseModel, Embeddings):
         })
         if server_info.type == LLMServerType.QWEN.value:
             params = {
-                "dashscope_api_key": params.get('openai_api_key'),
-                "model": params.get('model'),
+                'dashscope_api_key': params.get('openai_api_key'),
+                'model': params.get('model'),
             }
         elif server_info.type == LLMServerType.QIAN_FAN.value:
             params = {
-                "qianfan_ak": params.get("wenxin_api_key"),
-                "qianfan_sk": params.get("wenxin_secret_key"),
-                "model": params.get('model'),
+                'qianfan_ak': params.get('wenxin_api_key'),
+                'qianfan_sk': params.get('wenxin_secret_key'),
+                'model': params.get('model'),
             }
-        elif server_info.type in [LLMServerType.XINFERENCE.value, LLMServerType.LLAMACPP.value,
-                                  LLMServerType.VLLM.value]:
-            params['openai_api_key'] = params.pop('openai_api_key', None) or "EMPTY"
+        elif server_info.type in [
+                LLMServerType.XINFERENCE.value, LLMServerType.LLAMACPP.value,
+                LLMServerType.VLLM.value
+        ]:
+            params['openai_api_key'] = params.pop('openai_api_key', None) or 'EMPTY'
         return params
 
     @wrapper_bisheng_model_limit_check
--- src/backend/bisheng/workflow/nodes/qa_retriever/qa_retriever.py
+++ src/backend/bisheng/workflow/nodes/qa_retriever/qa_retriever.py
@@ -1,7 +1,58 @@
+from bisheng.interface.initialize.loading import instantiate_vectorstore
+from bisheng.interface.vector_store.custom import MilvusWithPermissionCheck
+from bisheng.workflow.callback.event import OutputMsgData
 from bisheng.workflow.nodes.base import BaseNode
+from bisheng_langchain.chains.retrieval.retrieval_chain import RetrievalChain
 
 
 class QARetrieverNode(BaseNode):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 判断是单次还是批量
+        self._tab = self.node_data.tab['value']
+
+        # 是否输出结果给用户
+        self._output_user = self.node_params.get('output_user', False)
+
+        # 初始化输入
+        self._user_question = self.node_params.get('user_question', '')
+        self._qa_knowledge_id = self.node_params.get('qa_knowledge_id', [])
+        self._score = self.node_params.get('score', 0.6)
+
+        # 初始化retriever，运行中初始化
+        self._retriever = None
+
+    def _init_retriever(self):
+        if self._retriever:
+            return
+        # 向量数据库客户端初始化，当前使用Milvus，更合理的使用更通用的工厂方法
+        params = {}
+        params['search_kwargs'] = {'k': 1, 'score_threshold': self._score}
+        params['search_type'] = 'similarity_score_threshold'
+        knowledge_retriever = instantiate_vectorstore(
+            node_type='MilvusWithPermissionCheck',
+            class_object=MilvusWithPermissionCheck,
+            params=params,
+        )
+
+        self._retriever = RetrievalChain(retriever=knowledge_retriever)
+
     def _run(self, unique_id: str):
-        pass
+        self._init_retriever()
+        question = self.graph_state.get_variable_by_str(self._user_question)
+        result = self._retriever.invoke({'query': question})
+        # qa 结果是document
+        if result['result']:
+            result_str = result['result'][0].page_content
+        else:
+            result_str = 'None'
+        if self._output_user:
+            self.callback_manager.on_output_msg(
+                OutputMsgData(node_id=self.id,
+                              msg=result_str,
+                              unique_id=unique_id,
+                              output_key=self._user_question))
+        ret = {}
+        ret['retrieval_result'] = result_str
+        return ret
