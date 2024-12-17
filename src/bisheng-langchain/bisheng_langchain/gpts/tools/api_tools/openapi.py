9e30f42124dfcb945a986d48251dfd39f19debb3
GuoQing Zhang <zhangguoqing@dataelem.com>
2024-12-17 18:54:41 +0800

    feat: agent node support sql_agent tool

--- src/bisheng-langchain/bisheng_langchain/gpts/tools/api_tools/openapi.py
+++ src/bisheng-langchain/bisheng_langchain/gpts/tools/api_tools/openapi.py
@@ -43,6 +43,8 @@ class OpenApiTools(APIToolBase):
                 field_type = int
             elif field_type == 'string':
                 field_type = str
+            elif field_type == 'boolean':
+                field_type = bool
             elif field_type in {'object', 'dict'}:
                 param_object_param = {}
                 for param in one['schema']['properties'].keys():
@@ -53,6 +55,8 @@ class OpenApiTools(APIToolBase):
                         field_type = int
                     elif field_type == 'string':
                         field_type = str
+                    elif field_type == 'boolean':
+                        field_type = bool
                     param_object_param[param] = (
                         field_type,
                         Field(description=one['schema']['properties'][param]['description']))
