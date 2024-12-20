2c053d547fc5e1aa75a523dffca7b1125053f3b2
GuoQing Zhang <zhangguoqing@dataelem.com>
2024-12-20 15:23:06 +0800

    feat: change prompt

--- src/bisheng-langchain/bisheng_langchain/gpts/tools/sql_agent/tool.py
+++ src/bisheng-langchain/bisheng_langchain/gpts/tools/sql_agent/tool.py
@@ -191,28 +191,7 @@ class SqlAgentAPIWrapper(BaseModel):
 
     def init_query_gen(self):
         # Add a node for a model to generate a query based on the question and schema
-        query_gen_system = """You are a SQL expert with a strong attention to detail.
-
-        Given an input question, output a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
-
-        DO NOT call any tool besides SubmitFinalAnswer to submit the final answer.
-
-        When generating the query:
-
-        Output the SQL query that answers the input question without a tool call.
-
-        Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
-        You can order the results by a relevant column to return the most interesting examples in the database.
-        Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-
-        If you get an error while executing a query, rewrite the query and try again.
-
-        If you get an empty result set, you should try to rewrite the query to get a non-empty result set. 
-        NEVER make stuff up if you don't have enough information to answer the query... just say you don't have enough information.
-
-        If you have enough information to answer the input question, simply invoke the appropriate tool to submit the final answer to the user.
-
-        DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database."""
+        query_gen_system = """You are a SQL expert with a strong attention to detail.Given an input question, output a syntactically correct SQL query to run, then look at the results of the query and return the answer.DO NOT call any tool besides SubmitFinalAnswer to submit the final answer.When generating the query:Output the SQL query that answers the input question without a tool call.Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 10 results.You can order the results by a relevant column to return the most interesting examples in the database.Never query for all the columns from a specific table, only ask for the relevant columns given the question.If you get an error while executing a query, rewrite the query and try again.If you get an empty result set, you should try to rewrite the query to get a non-empty result set. NEVER make stuff up if you don't have enough information to answer the query... just say you don't have enough information.If you have enough information to answer the input question, simply invoke the appropriate tool to submit the final answer to the user.DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database."""
         query_gen_prompt = ChatPromptTemplate.from_messages(
             [("system", query_gen_system), ("placeholder", "{messages}")]
         )
@@ -250,7 +229,7 @@ class SqlAgentAPIWrapper(BaseModel):
 
 
 class SqlAgentInput(BaseModel):
-    query: str = Field(description="用户数据查询需求的自然语言描述（需要尽可能完整、准确）")
+    query: str = Field(description="用户数据查询需求（需要尽可能完整、准确）")
 
 
 class SqlAgentTool(BaseTool):
