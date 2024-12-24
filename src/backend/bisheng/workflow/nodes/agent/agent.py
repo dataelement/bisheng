dfabfb4927694471b2a7dcbf11ec7d62e5f60aa2
GuoQing Zhang <zhangguoqing@dataelem.com>
2024-12-24 16:27:51 +0800

    feat: agent node delete chat history flag

--- src/backend/bisheng/workflow/nodes/agent/agent.py
+++ src/backend/bisheng/workflow/nodes/agent/agent.py
@@ -40,7 +40,7 @@ class AgentNode(BaseNode):
         self._tool_invoke_list = []
 
         # 聊天消息
-        self._chat_history_flag = self.node_params['chat_history_flag']['flag']
+        self._chat_history_flag = self.node_params['chat_history_flag']['value'] != 0
         self._chat_history_num = self.node_params['chat_history_flag']['value']
 
         self._llm = LLMService.get_bisheng_llm(model_id=self.node_params['model_id'],
