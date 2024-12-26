f308e4e6e1f200525ec2be35af4f8437f1b7a6ed
GuoQing Zhang <zhangguoqing@dataelem.com>
2024-12-26 15:00:13 +0800

    fix: change source documents data struct

--- src/backend/bisheng/workflow/callback/event.py
+++ src/backend/bisheng/workflow/callback/event.py
@@ -36,7 +36,7 @@ class OutputMsgData(BaseModel):
     msg: str = Field('', description='Output msg')
     files: List[dict] = Field([], description='Output files', exclude=True)
     output_key: str = Field(..., description='Whether the message is stream')
-    source_documents: Optional[List[Any]] = Field([], description='Source documents')
+    source_documents: Optional[Any] = Field(None, description='Source documents')
 
 
 class OutputMsgInputData(OutputMsgData):
