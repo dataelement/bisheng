87c7344a2582d2dcaf4b1e93798cb9ff0b87272e
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-02-18 17:25:21 +0800

    feat: business support set promise file

--- src/backend/bisheng/api/router.py
+++ src/backend/bisheng/api/router.py
@@ -3,7 +3,7 @@ from bisheng.api.v1 import (assistant_router, audit_router, chat_router, compone
                             endpoints_router, evaluation_router, finetune_router, flows_router,
                             group_router, knowledge_router, llm_router, mark_router, qa_router,
                             report_router, server_router, skillcenter_router, tag_router,
-                            user_router, validate_router, variable_router, workflow_router)
+                            user_router, validate_router, variable_router, workflow_router, promise_router)
 from bisheng.api.v2 import (assistant_router_rpc, chat_router_rpc, flow_router,
                             knowledge_router_rpc, rpc_router_rpc, workflow_router_rpc)
 from fastapi import APIRouter
@@ -30,6 +30,7 @@ router.include_router(tag_router)
 router.include_router(llm_router)
 router.include_router(workflow_router)
 router.include_router(mark_router)
+router.include_router(promise_router)
 
 router_rpc = APIRouter(prefix='/api/v2', )
 router_rpc.include_router(knowledge_router_rpc)
---
+++ src/backend/bisheng/api/v1/schema/promise.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class BusinessUserPromise(BaseModel):
+    business_id: str = Field(default=None, description='业务唯一标识')
+    promise_id: str = Field(default=None, description='承诺书唯一标识')
+    user_id: str = Field(default=None, description='用户ID')
+    write: bool = Field(default=False, description='是否已签署承诺书')
+    create_time: Optional[datetime] = Field(default=None, description='签署时间')
