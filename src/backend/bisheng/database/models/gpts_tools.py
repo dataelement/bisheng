9a372f0c76cecac95c02876aa476f0f76847f8e4
kratos <jingboshuo@gmail.com>
2025-02-20 11:52:36 +0800

    fix: leng
    add max length

--- src/backend/bisheng/database/models/gpts_tools.py
+++ src/backend/bisheng/database/models/gpts_tools.py
@@ -46,7 +46,7 @@ class GptsToolsTypeBase(SQLModelSerializable):
     description: str = Field(default='', description="工具类别的描述")
     server_host: Optional[str] = Field(default='', description="自定义工具的访问根地址，必须以http或者https开头")
     auth_method: Optional[int] = Field(default=0, description="工具类别的鉴权方式")
-    api_key: Optional[str] = Field(default='', description="工具鉴权的api_key",sa_column=Column(String(length=2048)))
+    api_key: Optional[str] = Field(default='', description="工具鉴权的api_key",sa_column=Column(String(length=2048)),max_length=1000)
     auth_type: Optional[str] = Field(default=AuthType.BASIC.value, description="工具鉴权的鉴权方式")
     is_preset: Optional[int] = Field(default=0, description="是否是预置工具类别")
     user_id: Optional[int] = Field(index=True, description='创建用户ID， null表示系统创建')
