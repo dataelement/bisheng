6a33a0e221928f0085c2ad8804cda59513e2cc5b
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-02-21 17:01:34 +0800

    fix: celery beat in linux only is utc

--- src/backend/entrypoint.sh
+++ src/backend/entrypoint.sh
@@ -2,7 +2,7 @@ nohup uvicorn bisheng.main:app --host 0.0.0.0 --port 7860 --no-access-log --work
 
 
 # 启动定时任务进程，注意此进程只需要启动一个即可。启动多次会导致定时任务重复执行
-nohup celery -A bisheng.worker.main beat -l info &
+nohup celery -A bisheng.worker.main beat --pidfile= -l info &
 
 # 启动异步任务处理worker -c 是指定celery的并发数
 celery -A bisheng.worker.main worker -l info -c 8
