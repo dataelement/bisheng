b6159827b1952b5c5b93fc018fd2d28e980e5988
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-03-05 11:37:14 +0800

    ci: change version

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-old_version="0.4.1.1"
-new_version="0.4.1.2"
+old_version="0.4.1.2"
+new_version="1.0.0"
 sed -i "s/$old_version/$new_version/g" ./docker/docker-compose.yml
 sed -i "s/$old_version/$new_version/g" ./src/backend/pyproject.toml
 sed -i "s/$old_version/$new_version/g" ./src/backend/bisheng/__init__.py
