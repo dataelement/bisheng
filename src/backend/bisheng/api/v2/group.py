5d49d579d79f450e1384d4e7cab51453b05f68ce
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-03-13 11:02:56 +0800

    fix: sync third department data into group

--- src/backend/bisheng/api/v2/group.py
+++ src/backend/bisheng/api/v2/group.py
@@ -2,13 +2,15 @@ from typing import List, Dict
 
 from fastapi import APIRouter, Request
 
+from bisheng.api.services.role_group_service import RoleGroupService
 from bisheng.api.v1.schemas import resp_200
 
-router = APIRouter(prefix='/group', tags=['OpenAPI', 'Workflow'])
+router = APIRouter(prefix='/group', tags=['OpenAPI', 'Group'])
+
 
 @router.post('/sync')
 async def sync_group(request: Request,
                      data: List[Dict]):
     """ 从其他第三方同步用户组信息 """
-
-    return resp_200(data)
+    RoleGroupService().sync_third_groups(data)
+    return resp_200()
