8772284de4fb8200d51783f8a38bc3e07a9340f2
leecolin0607 <1017519632@qq.com>
2025-04-23 11:19:27 +0800

    定时更新模型状态

--- src/backend/bisheng/worker/__init__.py
+++ src/backend/bisheng/worker/__init__.py
@@ -3,3 +3,4 @@ from bisheng.worker.test.test import *
 from bisheng.worker.knowledge.file_worker import *
 from bisheng.worker.audit.tasks import review_session_message
 from bisheng.worker.workflow.tasks import *
+from bisheng.worker.model.check_models import check_model_status_task
--- src/backend/bisheng/worker/main.py
+++ src/backend/bisheng/worker/main.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from bisheng.interface.utils import setup_llm_caching
 
@@ -6,3 +7,10 @@ setup_llm_caching()
 
 bisheng_celery = Celery('bisheng', include=['bisheng.worker'])
 bisheng_celery.config_from_object('bisheng.worker.config')
+bisheng_celery.conf.beat_schedule = {
+    'check_model_status_task': {
+        'task': 'bisheng.worker.model.check_models.check_model_status_task',
+        'schedule': crontab(minute='*/1'),
+    }
+}
+
