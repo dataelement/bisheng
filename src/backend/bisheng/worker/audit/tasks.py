0070733319d8a1ca96c5d568b28dd198c5dbd647
leecolin0607 <1017519632@qq.com>
2025-04-30 00:58:58 +0800

    定时任务执行记录入库

--- src/backend/bisheng/worker/audit/tasks.py
+++ src/backend/bisheng/worker/audit/tasks.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime, timedelta
 
 from loguru import logger
@@ -5,14 +6,25 @@ from loguru import logger
 from bisheng.api.services.audit_log import AuditLogService
 from bisheng.api.v1.schema.audit import DayCron
 from bisheng.database.models.message import MessageDao
+from bisheng.database.models.scheduled_task_logs import ScheduledTaskLogs, ScheduledTaskLogsDao, LogType
 from bisheng.worker import bisheng_celery
+task_name = "review_session_message"
+def insert_start(task_id):
+    data = ScheduledTaskLogs(task_id=task_id,task_name=task_name,log_type=LogType.STARTED.value)
+    ScheduledTaskLogsDao.insert_one(data)
 
+def insert_finish(task_id,review_num,start_date,end_date):
+    log_content = {"review_num":review_num,"start_date":start_date,"end_date":end_date}
+    data = ScheduledTaskLogs(task_id=task_id,task_name=task_name,log_type=LogType.FINISHED.value,log_content=log_content)
+    ScheduledTaskLogsDao.insert_one(data)
 
 @bisheng_celery.task
 def review_session_message():
     """
     审查会话产生的消息
     """
+    task_id = str(uuid.uuid4())
+    insert_start(task_id)
     session_config = AuditLogService.get_session_config()
     if not session_config.flag:
         # 未开启审查，则跳过
@@ -28,13 +40,15 @@ def review_session_message():
     # 获取期间有更新的会话
     page = 1
     page_size = 100
+    review_num = 0
     while True:
         res, total = MessageDao.app_list_group_by_chat_id(page_size, page, start_date=start_date, end_date=end_date)
         if len(res) == 0:
             break
         for one in res:
             AuditLogService.review_one_session(one['chat_id'])
+        review_num += len(res)
         page += 1
-
+    insert_finish(task_id,review_num,start_date,end_date)
     logger.debug(f'review_session_message over start_date={start_date} end_date={end_date}')
 
