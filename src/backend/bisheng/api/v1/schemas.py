696a083dc94af2a066fb77090b15137a9dd38815
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-05-07 17:16:19 +0800

    feat: update flow version info support empty name

--- src/backend/bisheng/api/v1/schemas.py
+++ src/backend/bisheng/api/v1/schemas.py
@@ -301,7 +301,7 @@ class AssistantInfo(AssistantBase):
 
 
 class FlowVersionCreate(BaseModel):
-    name: Optional[str] = Field(default=..., description='版本的名字')
+    name: Optional[str] = Field(default=None, description='版本的名字')
     description: Optional[str] = Field(default=None, description='版本的描述')
     data: Optional[Dict] = Field(default=None, description='技能版本的节点数据数据')
     original_version_id: Optional[int] = Field(default=None, description='版本的来源版本ID')
