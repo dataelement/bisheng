4c3bb7f3422c266cdc8cf73e1f8844879d8c0e96
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-05-20 10:52:54 +0800

    ci: change uns version

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 old_version="1.1.1"
 new_version="1.2.0"
-sed -i .bak "s/$old_version/$new_version/g" ./docker/docker-compose.yml
-sed -i .bak "s/$old_version/$new_version/g" ./src/backend/pyproject.toml
-sed -i .bak "s/$old_version/$new_version/g" ./src/backend/bisheng/__init__.py
+sed -i.bak "s/$old_version/$new_version/g" ./docker/docker-compose.yml
+sed -i.bak "s/$old_version/$new_version/g" ./src/backend/pyproject.toml
+sed -i.bak "s/$old_version/$new_version/g" ./src/backend/bisheng/__init__.py
