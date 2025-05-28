17cd180c151a422232491d3122255957e1ea0b17
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-05-28 20:21:26 +0800

    fix: entrypoint

--- docker/bisheng/entrypoint.sh
+++ docker/bisheng/entrypoint.sh
@@ -2,10 +2,10 @@
 
 start_mode=${1:-api}
 
-if [ "$start_mode" == "api" ]; then
+if [ $start_mode = "api" ]; then
     echo "Starting API server..."
     uvicorn bisheng.main:app --host 0.0.0.0 --port 7860 --no-access-log --workers 8
-elif [ "$start_mode" == "celery" ]; then
+elif [ $start_mode = "worker" ]; then
     echo "Starting Celery worker..."
     # -c 是指定celery的并发数，线程数
     celery -A bisheng.worker.main worker -l info -c 100 -P threads
