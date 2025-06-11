2781dd2b760e69cfcb481fc5c024605719e05991
tju <tony.ju@me.com>
2025-06-11 22:40:39 +0800

    add patch 130

--- src/backend/bisheng/api/services/md_from_pptx.py
+++ src/backend/bisheng/api/services/md_from_pptx.py
@@ -32,7 +32,7 @@ def handler(
 ):
     doc_id = str(uuid4())
     md_file_name = f"{cache_dir}/{doc_id}.md"
-    image_dir = f"{cache_dir}/images"
+    image_dir = f"{cache_dir}/{doc_id}"
     parser_pptx2md(
         pptx_file=file_name,
         md_file=md_file_name,
@@ -41,3 +41,13 @@ def handler(
     # 上传图片,可以用异步
     # 替换md文件中的图片路径
     return md_file_name, image_dir, doc_id
+
+
+if __name__ == "__main__":
+    pptx_file = "/Users/tju/Resources/docs/ppt/you-lian.pptx"
+    cache_dir = "/Users/tju/Desktop"
+
+    md_file_name, image_dir, doc_id = handler(cache_dir, pptx_file)
+    print(f"Markdown file: {md_file_name}")
+    print(f"Image directory: {image_dir}")
+    print(f"Document ID: {doc_id}")
