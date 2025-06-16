5803b02ac6f08da171eb8586ae252832f198a838
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-06-16 14:57:24 +0800

    fix: excel document error

--- src/backend/bisheng/api/services/patch_130.py
+++ src/backend/bisheng/api/services/patch_130.py
@@ -6,13 +6,13 @@ from bisheng.api.services.md_from_docx import handler as docx_handler
 from bisheng.api.services.md_from_excel import handler as excel_handler
 from bisheng.api.services.md_from_html import handler as html_handler
 from bisheng.api.services.md_from_pptx import handler as pptx_handler
+from bisheng.api.services.md_post_processing import post_processing
 from bisheng.cache.utils import CACHE_DIR
 from bisheng.utils.minio_client import minio_client
-from bisheng.api.services.md_post_processing import post_processing
 
 
 def combine_multiple_md_files_to_raw_texts(
-    path,
+        path,
 ) -> tuple[list[Document], list[Document]]:
     """
     combine multiple md file to raw texts including meta-data list.
@@ -25,26 +25,27 @@ def combine_multiple_md_files_to_raw_texts(
 
     files = sorted([f for f in os.listdir(path)])
     raw_texts = []
-    documents = []
+
+    # 一个文件只对应一个完整的 Document 对象, texts 才是切分后的chunk内容
+    documents = [Document(page_content="", metadata={})]
+
     for file_name in files:
-        document = Document(page_content="", metadata={})
         full_file_name = f"{path}/{file_name}"
         with open(full_file_name, "r", encoding="utf-8") as f:
             content = f.read()
-            document.page_content += content + "\n"
             raw_texts.append(Document(page_content=content, metadata={}))
-            documents.append(document)
+            documents[0].page_content += content
     return raw_texts, documents
 
 
 def convert_file_to_md(
-    file_name,
-    input_file_name,
-    header_rows=[0, 1],
-    data_rows=10,
-    append_header=True,
-    knowledge_id=None,
-    retain_images=True,
+        file_name,
+        input_file_name,
+        header_rows=[0, 1],
+        data_rows=10,
+        append_header=True,
+        knowledge_id=None,
+        retain_images=True,
 ):
     """
     处理文件转换的主函数。
@@ -66,18 +67,18 @@ def convert_file_to_md(
         md_file_name, local_image_dir, doc_id = pptx_handler(CACHE_DIR, input_file_name)
         include_cache_dir = False
     elif (
-        file_name.endswith(".xlsx")
-        or file_name.endswith(".xls")
-        or file_name.endswith(".csv")
+            file_name.endswith(".xlsx")
+            or file_name.endswith(".xls")
+            or file_name.endswith(".csv")
     ):
         md_file_name, local_image_dir, doc_id = excel_handler(
             CACHE_DIR, input_file_name, header_rows, data_rows, append_header
         )
         local_image_dir = None
     elif (
-        file_name.endswith(".html")
-        or file_name.endswith(".htm")
-        or file_name.endswith(".mhtml")
+            file_name.endswith(".html")
+            or file_name.endswith(".htm")
+            or file_name.endswith(".mhtml")
     ):
         (
             md_file_name,
@@ -98,12 +99,12 @@ def convert_file_to_md(
 
 
 def replace_image_url(
-    md_file_name,
-    local_image_dir,
-    doc_id,
-    include_cache_dir,
-    knowledge_id=None,
-    retain_images=True,
+        md_file_name,
+        local_image_dir,
+        doc_id,
+        include_cache_dir,
+        knowledge_id=None,
+        retain_images=True,
 ):
     """
     Usage:
