602dc2e8c3e0db2d3c5601f281588425da956722
Jerry <82269916@qq.com>
2025-06-22 01:12:46 +0800

    1.3.0合并-添加脚本

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 which tar
 
-tar --exclude='__pycache__' --exclude='node_modules' --exclude='src/backend/venv' -czf bisheng.tar.gz .drone.yml .gitignore docker/bisheng/config/config.yaml docker/bisheng/entrypoint.sh docker/docker-compose.yml src/backend src/frontend
+tar --exclude='__pycache__' --exclude='node_modules' --exclude='src/backend/venv' --exclude='src/backend/poetry.toml' --exclude='src/backend/venv' -czf bisheng.tar.gz .drone.yml .gitignore docker/bisheng/config/config.yaml docker/bisheng/entrypoint.sh docker/docker-compose.yml src/backend src/frontend
