21d34fd379e0fea3f9f90b9e0a670fd3c9122f00
flowerheap <flowerheap@163.com>
2025-06-21 23:09:38 +0800

    feat: 合并代码

--- src/frontend/platform/src/pages/BuildPage/flow/FlowChat/ChatInput.tsx
+++ src/frontend/platform/src/pages/BuildPage/flow/FlowChat/ChatInput.tsx
@@ -20,12 +20,8 @@ import SpeechToTextComponent from "@/components/SpeechToTextComponent";
 export const FileTypes = {
     ALL: ['.PNG', '.JPEG', '.JPG', '.BMP', '.PDF', '.TXT', '.MD', '.HTML', '.XLS', '.XLSX', '.CSV', '.DOC', '.DOCX', '.PPT', '.PPTX'],
     IMAGE: ['.PNG', '.JPEG', '.JPG', '.BMP'],
-<<<<<<< HEAD
     FILE: ['.PDF', '.TXT', '.MD', '.HTML', '.XLS', '.XLSX', '.DOC', '.CSV', '.DOCX', '.PPT', '.PPTX'],
     AUDIO: ['.MP3', '.AMR', 'WAV', 'AAC'],
-=======
-    FILE: ['.PDF', '.TXT', '.MD', '.HTML', '.XLS', '.XLSX', '.CSV', '.DOC', '.DOCX', '.PPT', '.PPTX'],
->>>>>>> v1.3.0
 }
 
 export default function ChatInput({ autoRun, v = 'v1', clear, form, wsUrl, onBeforSend, onLoad, flow }) {
