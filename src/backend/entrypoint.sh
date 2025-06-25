f136dca12132fea2364d0a03a99dcac0981ba5eb
relzhong <relzhong@outlook.com>
2025-06-25 15:29:53 +0800

    feat: add celery worker name

--- src/backend/entrypoint.sh
+++ src/backend/entrypoint.sh
@@ -8,9 +8,9 @@ if [ $start_mode = "api" ]; then
 elif [ $start_mode = "worker" ]; then
     echo "Starting Celery worker..."
     # 处理知识库相关任务的worker
-    nohup celery -A bisheng.worker.main worker -l info -c 20 -P threads -Q knowledge_celery &
+    nohup celery -A bisheng.worker.main worker -l info -c 20 -P threads -Q knowledge_celery -n knowledge@%h &
     # 工作流执行worker
-    celery -A bisheng.worker.main worker -l info -c 100 -P threads -Q workflow_celery
+    celery -A bisheng.worker.main worker -l info -c 100 -P threads -Q workflow_celery -n workflow@%h
 else
     echo "Invalid start mode. Use 'api' or 'celery'."
     exit 1
