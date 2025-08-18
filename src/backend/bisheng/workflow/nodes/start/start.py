b8181cb22ed5de405d8f15a4e23398751655004a
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-08-18 17:41:31 +0800

    feat: 工作流开始节点增加user_info变量

--- src/backend/bisheng/workflow/nodes/start/start.py
+++ src/backend/bisheng/workflow/nodes/start/start.py
@@ -1,9 +1,15 @@
 import datetime
+import json
 from typing import Any, Dict
 
 from langchain.memory import ConversationBufferWindowMemory
 
 from bisheng.chat.types import IgnoreException
+from bisheng.database.models.group import GroupDao
+from bisheng.database.models.role import RoleDao
+from bisheng.database.models.user import UserDao
+from bisheng.database.models.user_group import UserGroupDao
+from bisheng.database.models.user_role import UserRoleDao
 from bisheng.workflow.callback.event import GuideQuestionData, GuideWordData
 from bisheng.workflow.nodes.base import BaseNode
 
@@ -18,8 +24,22 @@ class StartNode(BaseNode):
         # 初始化聊天历史记录
         self.graph_state.history_memory = ConversationBufferWindowMemory(
             k=self.node_params.get('chat_history', 10))
+        self._user_info = None
 
     def _run(self, unique_id: str) -> Dict[str, Any]:
+        if self._user_info is None and self.user_id:
+            user_db = UserDao.get_user(int(self.user_id))
+            user_group = UserGroupDao.get_user_group(int(self.user_id))
+            groups = GroupDao.get_group_by_ids([group.group_id for group in user_group])
+            user_role = UserRoleDao.get_user_roles(int(self.user_id))
+            roles = RoleDao.get_role_by_ids([role.role_id for role in user_role])
+            if user_db:
+                self._user_info = json.dumps({
+                    "user_name": user_db.user_name,
+                    "user_group": [group.group_name for group in groups],
+                    "user_role": [one.role_name for one in roles]
+                }, ensure_ascii=False, indent=2)
+
         if self.node_params['guide_word']:
             self.callback_manager.on_guide_word(
                 data=GuideWordData(node_id=self.id, unique_id=unique_id, guide_word=self.node_params['guide_word']))
@@ -38,7 +58,8 @@ class StartNode(BaseNode):
         return {
             'current_time': self.node_params['current_time'],
             'chat_history': '',
-            'preset_question': new_preset_question
+            'preset_question': new_preset_question,
+            'user_info': self._user_info,
         }
 
     def parse_log(self, unique_id: str, result: dict) -> Any:
@@ -51,6 +72,11 @@ class StartNode(BaseNode):
             {
                 "key": "preset_question",
                 "value": result['preset_question'],
+                "type": "params"
+            },
+            {
+                "key": "user_info",
+                "value": result['user_info'],
                 "type": "params"
             }
         ]]
