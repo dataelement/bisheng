e37d66b7d70ff7579248997f78bb5fac650bd9b2
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-08-19 19:51:33 +0800

    feat: 工具参数修改bool的转换逻辑

--- src/backend/bisheng_langchain/utils/openapi.py
+++ src/backend/bisheng_langchain/utils/openapi.py
@@ -17,7 +17,7 @@ def convert_openapi_field_value(v: Any, field_type: str) -> Any:
     elif field_type == 'integer' and v_type != int:
         return convert_data_type_no_error(v, int)
     elif field_type == 'boolean' and v_type != bool:
-        return convert_data_type_no_error(v, bool)
+        return str(v).strip().lower() == 'true'
     elif field_type == 'string' and v_type != str:
         return convert_data_type_no_error(v, str)
     elif field_type == 'array' and v_type != list:
