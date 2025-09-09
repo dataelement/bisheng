bb2c6caead30ad1f9936875826bcba16abb0cb10
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-09-09 17:44:13 +0800

    fix: 开始节点修改执行用户的角色信息

--- src/backend/bisheng/workflow/nodes/start/start.py
+++ src/backend/bisheng/workflow/nodes/start/start.py
@@ -31,18 +31,25 @@ class StartNode(BaseNode):
             user_db = UserDao.get_user(int(self.user_id))
             user_group = UserGroupDao.get_user_group(int(self.user_id))
             groups = GroupDao.get_group_by_ids([group.group_id for group in user_group])
+            group_dict = {group.id: group.group_name for group in groups}
             user_role = UserRoleDao.get_user_roles(int(self.user_id))
             roles = RoleDao.get_role_by_ids([role.role_id for role in user_role])
+            user_roles = []
+            for one in roles:
+                if one.group_id not in group_dict.keys():
+                    continue
+                user_roles.append(f"{group_dict[one.group_id]}-{one.role_name}")
             if user_db:
                 self._user_info = json.dumps({
                     "user_name": user_db.user_name,
-                    "user_group": [group.group_name for group in groups],
-                    "user_role": [one.role_name for one in roles]
+                    "user_group": list(group_dict.values()),
+                    "user_role": user_roles,
                 }, ensure_ascii=False, indent=2)
 
         if self.node_params['guide_word']:
             self.callback_manager.on_guide_word(
-                data=GuideWordData(node_id=self.id, name=self.name, unique_id=unique_id, guide_word=self.node_params['guide_word']))
+                data=GuideWordData(node_id=self.id, name=self.name, unique_id=unique_id,
+                                   guide_word=self.node_params['guide_word']))
         if self.node_params['guide_question']:
             self.callback_manager.on_guide_question(
                 data=GuideQuestionData(node_id=self.id, name=self.name, unique_id=unique_id,
