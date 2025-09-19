b375bd7001c95e31986a430c7da521737a9ef353
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-09-19 18:44:40 +0800

    fix: 修改错误码字段问题

--- src/backend/bisheng/api/errcode/chat.py
+++ src/backend/bisheng/api/errcode/chat.py
@@ -5,13 +5,13 @@ from .base import BaseErrorCode
 # 该技能已被删除
 class SkillDeletedError(BaseErrorCode):
     code = 13001
-    message = "该技能已被删除"
+    Msg = "该技能已被删除"
 
 
 # 当前技能未上线，无法直接对话
 class SkillNotOnlineError(BaseErrorCode):
     code = 13002
-    message = "当前技能未上线，无法直接对话"
+    Msg = "当前技能未上线，无法直接对话"
 
 
 # 当前编译没通过
@@ -31,10 +31,12 @@ class LLMExecutionError(BaseErrorCode):
     Code = 13005
     Msg = "LLM 技能执行错误. error={error}"
 
+
 # 文档解析失败，点击输入框上传按钮重新上传\n\n{str(e)}
 class DocumentParseError(BaseErrorCode):
     Code = 13006
     Msg = "文档解析失败，点击输入框上传按钮重新上传\n\n{error}"
+
 
 # Input data is parsed fail. error={str(e)}
 class InputDataParseError(BaseErrorCode):
