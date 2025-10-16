1b57d033483e0e1471afa9516e160f0b4b20d6c2
lmr <350416710@qq.com>
2025-10-16 14:26:39 +0800

    fix:fix bug

--- src/frontend/platform/src/workspace/FilePreviewDrawer.tsx
+++ src/frontend/platform/src/workspace/FilePreviewDrawer.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { Sheet, SheetContent, SheetHeader } from '@/components/bs-ui/sheet'
-import { ChevronLeft, Download } from 'lucide-react'
+import { CheckCircle, ChevronLeft, CircleX, Download, Loader2 } from 'lucide-react'
 import type React from "react"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { useTranslation } from 'react-i18next'
 import FilePreview from './FilePreview'
 import { Button } from '@/components/bs-ui/button'
@@ -43,11 +43,12 @@ export default function FilePreviewDrawer({
     downloadFile,
     directFile,
     onBack,
-    handleExportOther
+    handleExportOther,
+    exportState
 }: FilePreviewDrawerProps) {
     const { t: localize } = useTranslation()
     // const [selectedFileId, setSelectedFileId] = useState(currentFileId || files?.[0]?.file_id || "")
-
+    const [tooltipOpen, setTooltipOpen] = useState(false)
     // 获取文件扩展名
     const getFileExtension = (fileName: string): string => {
         const lastDot = fileName.lastIndexOf(".")
@@ -81,6 +82,25 @@ export default function FilePreviewDrawer({
     return (
         <Sheet open={isOpen} onOpenChange={onOpenChange}>
             <SheetContent className="w-[800px] sm:max-w-[800px] p-0">
+            {exportState.loading && (
+                    <div className="fixed top-24 right-5 flex items-center gap-2 bg-white p-3 rounded-lg shadow-md z-500">
+                        <Loader2 className="size-5 animate-spin text-blue-500" />
+                        <div className="text-sm text-gray-800">{exportState.title}&nbsp;正在导出，请稍后...&nbsp;&nbsp;</div>
+                    </div>
+                )}
+                {exportState.success && (
+                    <div className="fixed top-24 right-5 flex items-center gap-2 bg-white p-3 rounded-lg shadow-md z-500">
+                        <CheckCircle className="size-5 text-green-500" />
+                        <div className="text-sm text-gray-800">{exportState.title}&nbsp;文件下载成功</div>
+                    </div>
+                )}
+                {exportState.error && (
+                    <div className="fixed top-24 right-5 flex items-center gap-2 bg-white p-3 rounded-lg shadow-md z-500">
+                        <CircleX className="size-5 text-red-500" />
+                        <div className="text-sm text-gray-800">导出失败</div>
+                    </div>
+                )}
+
                 <SheetHeader className="px-6 py-4">
                     <div className="flex items-center justify-between">
                         <div className="flex items-center space-x-3 flex-1">
@@ -108,23 +128,26 @@ export default function FilePreviewDrawer({
                                 {/* 下载按钮 */}
                                 <Button variant="ghost" disabled={!currentDisplayFile}>
                                         {String(currentDisplayFile.file_name).toLowerCase().endsWith('.md') ? (
-                                            <Tooltip>
+                                             <Tooltip
+                                             open={tooltipOpen}
+                                             onOpenChange={setTooltipOpen} // 绑定tooltip状态
+                                         >
                                                 <TooltipTrigger asChild>
                                                     <span onClick={(e) => e.stopPropagation()}>
-                                                        <Download size={16} />
+                                                        <Download size={16} onClick={()=>{setTooltipOpen(true)}}/>
                                                     </span>
                                                 </TooltipTrigger>
                                                 <TooltipContent side='bottom' align='center' className='bg-white text-gray-800 border border-gray-200'>
                                                     <div className='flex flex-col gap-2'>
-                                                        <div className='flex gap-2 items-center cursor-pointer hover:bg-gray-100 rounded-md p-1' onClick={(e) => { e.stopPropagation(); downloadFile(currentDisplayFile); }}>
+                                                        <div className='flex gap-2 items-center cursor-pointer hover:bg-gray-100 rounded-md p-1' onClick={(e) => { e.stopPropagation(); downloadFile(currentDisplayFile); setTooltipOpen(false); }}>
                                                             <FileIcon type={'md'} className='size-5' />
                                                             <div className='w-full flex gap-2 items-center'>Markdown</div>
                                                         </div>
-                                                        <div className='flex gap-2 items-center rounded-md p-1 cursor-pointer hover:bg-gray-100' onClick={(e) => handleExportOther(e, 'pdf',currentDisplayFile)}>
+                                                        <div className='flex gap-2 items-center rounded-md p-1 cursor-pointer hover:bg-gray-100' onClick={(e) => {e.stopPropagation();handleExportOther(e, 'pdf',currentDisplayFile); setTooltipOpen(false);}}>
                                                             <FileIcon type={'pdf'} className='size-5' />
                                                             <div className='w-full flex gap-2 items-center'>PDF</div>
                                                         </div>
-                                                        <div className='flex gap-2 items-center rounded-md p-1 cursor-pointer hover:bg-gray-100' onClick={(e) => handleExportOther(e, 'docx',currentDisplayFile)}>
+                                                        <div className='flex gap-2 items-center rounded-md p-1 cursor-pointer hover:bg-gray-100' onClick={(e) => {e.stopPropagation();handleExportOther(e, 'docx',currentDisplayFile); setTooltipOpen(false);}}>
                                                             <FileIcon type={'docx'} className='size-5' />
                                                             <div className='w-full flex gap-2 items-center'>Docx</div>
                                                         </div>
