84c1610c48bc51dadeed6caca18adeba6e5af97e
lmr <350416710@qq.com>
2025-11-17 20:04:08 +0800

    feat: 2.3beta1

--- src/frontend/platform/src/components/bs-ui/input/index.tsx
+++ src/frontend/platform/src/components/bs-ui/input/index.tsx
@@ -66,7 +66,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
                 <input
                     type={type}
                     className={cname(
-                        "flex h-9 w-full rounded-md border border-input bg-search-input px-3 py-1 text-sm text-[#111] dark:text-gray-50 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+                        "flex h-8 w-full rounded-md border border-input bg-search-input px-3 py-1 text-sm text-[#111] dark:text-gray-50 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
                         type === 'number' ? 'number-input-arrows' : '',
                         className
                     )}
