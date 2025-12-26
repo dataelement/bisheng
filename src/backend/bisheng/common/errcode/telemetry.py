24ad9363e2391cdc705cf7e0ba7609e22b1a9c4e
GuoQing Zhang <zhangguoqing@dataelem.com>
2025-12-26 16:40:53 +0800

    feat: add dashboard auth error code

--- src/backend/bisheng/common/errcode/telemetry.py
+++ src/backend/bisheng/common/errcode/telemetry.py
@@ -4,3 +4,8 @@ from bisheng.common.errcode import BaseErrorCode
 class DashboardMaxError(BaseErrorCode):
     Code: int = 17000
     Msg: str = '最多允许创建 20 个看板'
+
+
+class DashBoardShareAuthError(BaseErrorCode):
+    Code: int = 17010
+    Msg: str = '没有看板分享权限'
