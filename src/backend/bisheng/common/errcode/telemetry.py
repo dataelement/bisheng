a0c65dd020d7ea8d6439b43849f478d146c2cbbc
GuoQing Zhang <zhangguoqing@dataelem.com>
2026-01-04 18:44:46 +0800

    feat: add dashboard error code

--- src/backend/bisheng/common/errcode/telemetry.py
+++ src/backend/bisheng/common/errcode/telemetry.py
@@ -7,5 +7,35 @@ class DashboardMaxError(BaseErrorCode):
 
 
 class DashBoardShareAuthError(BaseErrorCode):
-    Code: int = 17010
+    Code: int = 17005
     Msg: str = '没有看板分享权限'
+
+
+class QueryDatasetNotFoundError(BaseErrorCode):
+    Code: int = 17010
+    Msg: str = '未找到对应的数据集配置'
+
+
+class QueryVirtualMaxError(BaseErrorCode):
+    Code: int = 17011
+    Msg: str = '虚拟指标只能单独查询一个'
+
+
+class QueryMetricNotFoundError(BaseErrorCode):
+    Code = 17012
+    Msg = '未找到对应的指标配置'
+
+
+class QueryAggregationNotFoundError(BaseErrorCode):
+    Code = 17013
+    Msg = '未找到对应的汇总方式'
+
+
+class QueryDimensionNotFoundError(BaseErrorCode):
+    Code = 17014
+    Msg = '未找到对应的维度配置'
+
+
+class QueryOperatorNotFoundError(BaseErrorCode):
+    Code = 17015
+    Msg = '未找到对应的操作符配置'
