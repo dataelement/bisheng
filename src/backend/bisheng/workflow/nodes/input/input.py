2099c40edb4b915a5204bfea176207284ca20024
GuoQing Zhang <zhangguoqing@dataelem.com>
2026-01-22 19:36:31 +0800

    feat: workflow input node support file parse mode

--- src/backend/bisheng/workflow/nodes/input/input.py
+++ src/backend/bisheng/workflow/nodes/input/input.py
@@ -2,7 +2,9 @@ import os
 import shutil
 import tempfile
 import time
-from typing import Any
+from enum import Enum
+from typing import Any, List
+from urllib.parse import urlparse, unquote
 
 from loguru import logger
 
@@ -10,12 +12,19 @@ from bisheng.api.services.knowledge import KnowledgeService
 from bisheng.api.services.knowledge_imp import read_chunk_text
 from bisheng.api.v1.schemas import FileProcessBase
 from bisheng.chat.types import IgnoreException
+from bisheng.common.errcode.knowledge import KnowledgeFileNotSupportedError
 from bisheng.core.cache.utils import file_download
 from bisheng.knowledge.domain.knowledge_rag import KnowledgeRag
 from bisheng.llm.domain.services import LLMService
 from bisheng.utils import generate_uuid
 from bisheng.workflow.nodes.base import BaseNode
 from bisheng.workflow.nodes.input.const import InputFileMetadata
+
+
+class ParseModeEnum(str, Enum):
+    KEEP_RAW = 'keep_raw'
+    EXTRACT_TEXT = 'extract_text'
+    INGEST_TO_KNOWLEDGE_BASE = 'ingest_to_temp_kb'
 
 
 class InputNode(BaseNode):
@@ -31,9 +40,9 @@ class InputNode(BaseNode):
         self._node_params_map = {}
         new_node_params = {}
         # The maximum length of the input file in the dialog box, more than this length will be truncated
-        self._dialog_files_length = int(self.node_params.get('dialog_files_content_size', 15000))
-        # save image file path
-        self._dialog_images_files = []
+
+        if self.node_data.v < self._current_v:
+            raise IgnoreException(f'{self.name} -- workflow node is update')
 
         if self.is_dialog_input():
             new_node_params['user_input'] = self.node_params['user_input']
@@ -46,15 +55,11 @@ class InputNode(BaseNode):
         self.node_params = new_node_params
         self._image_ext = ['png', 'jpg', 'jpeg', 'bmp']
 
-        self._embedding = None
         self._vector_client = None
         self._es_client = None
 
     def is_dialog_input(self):
         """ Whether the input is in the form of a conversation """
-        if self.node_data.v < self._current_v:
-            raise IgnoreException(f'{self.name} -- workflow node is update')
-
         if self._tab == 'dialog_input':
             return True
         elif self._tab == 'form_input':
@@ -77,11 +82,19 @@ class InputNode(BaseNode):
     def _run(self, unique_id: str):
         if self.is_dialog_input():
             # Input in the form of a dialog
-            dislog_files_content, self._dialog_images_files = self.parse_dialog_files()
+            result = self.parse_upload_file("dialog_files", {
+                "key": "dialog_files",
+                "file_content": "dialog_files_content",
+                "file_path": "dialog_files_path",
+                "image_file": "dialog_image_files",
+                "file_parse_mode": self.node_params.get("file_parse_mode", ParseModeEnum.EXTRACT_TEXT),
+                "file_content_size": self.node_params.get("dialog_files_content_size", 15000)
+            }, self.node_params.get('dialog_files_content', []))
             res = {
                 'user_input': self.node_params['user_input'],
-                'dialog_files_content': dislog_files_content,
-                'dialog_image_files': self._dialog_images_files
+                'dialog_files_content': result.get("dialog_files_content", ""),
+                'dialog_files_path': result.get("dialog_files_path", []),
+                'dialog_image_files': result.get("dialog_image_files", []),
             }
             self.graph_state.save_context(content=f'{res["dialog_files_content"]}\n{res["user_input"]}',
                                           msg_sender='human')
@@ -115,28 +128,6 @@ class InputNode(BaseNode):
             ret.append({"key": f'{self.id}.{k}', "value": v, "type": "variable"})
         return [ret]
 
-    def parse_dialog_files(self) -> (str, list[str]):
-        """ Get the file content uploaded in the dialog box """
-        file_length = 0
-        dialog_files_content = ""
-        image_files_path = []
-        if not self.node_params.get('dialog_files_content'):
-            return dialog_files_content, image_files_path
-        for file_id in self.node_params['dialog_files_content']:
-            file_name, file_path, chunks, _ = self.get_upload_file_path_content(file_id)
-            file_ext = file_name.split('.')[-1].lower()
-            if file_ext in self._image_ext:
-                image_files_path.append(file_path)
-
-            if file_length >= self._dialog_files_length:
-                continue
-            file_content = "\n".join(chunks)
-            file_content = file_content[:self._dialog_files_length - file_length]
-            file_length += len(file_content)
-
-            dialog_files_content += f"[file name]: {file_name}\n[file content begin]\n{file_content}\n[file content end]\n"
-        return dialog_files_content, image_files_path
-
     def get_upload_file_path_content(self, file_url: str) -> (str, str, list, list):
         """
         params:
@@ -164,70 +155,77 @@ class InputNode(BaseNode):
                                                      file_rule.retain_images, file_rule.enable_formula,
                                                      file_rule.force_ocr,
                                                      file_rule.filter_page_header_footer, file_rule.excel_rule)
-        except Exception as e:
+        except KnowledgeFileNotSupportedError as e:
             logger.exception('parse input node file error')
             if str(e).find('Type not supported') == -1:
                 raise e
 
         return file_name, original_file_path, texts, metadatas
 
-    def parse_upload_file(self, key: str, key_info: dict, value: str) -> dict | None:
-        """
-         Upload files tomilvusLate Stageろ
-         DocumentedmetadataData, full-text files, local file paths
-        """
-        # 1Get the defaultembeddingModels
-        if self._embedding is None:
+    def init_vector_clients(self):
+        if self._vector_client is None:
             embedding = LLMService.get_knowledge_default_embedding(self.user_id)
             if not embedding:
-                raise Exception('No default configuredembeddingModels')
-            self._embedding = embedding
-
-        if self._vector_client is None:
-            # 2InisialisasimilvusAndesInstances
-            milvus_collection_name = self.get_milvus_collection_name(getattr(self._embedding, 'model_id'))
-            self._vector_client = KnowledgeRag.init_milvus_vectorstore(milvus_collection_name, self._embedding,
+                raise Exception('No default configured embedding Models')
+            milvus_collection_name = self.get_milvus_collection_name(getattr(embedding, 'model_id'))
+            self._vector_client = KnowledgeRag.init_milvus_vectorstore(milvus_collection_name, embedding,
                                                                        metadata_schemas=InputFileMetadata)
             self._es_client = KnowledgeRag.init_es_vectorstore_sync(self.tmp_collection_name,
                                                                     metadata_schemas=InputFileMetadata)
 
-        if 'file_content' not in key_info:
-            raise IgnoreException(f'{self.name} -- workflow node is update')
-        if not value:
-            logger.warning(f"{self.id}.{key} value is None")
-            return {
-                key_info['key']: None,
-                key_info['file_content']: None,
-                key_info['file_path']: None,
-                key_info['image_file']: None
-            }
-
-        # Parsing the file.
+    def parse_upload_file(self, key: str, key_info: dict, value: List[str]) -> dict | None:
+        """
+         parse upload_file
+         Documented metadataData, full-text files, minio file paths, image files path
+        """
+        # Parsing the file. need return values
         all_metadata = []
         all_file_content = ''
         original_file_path = []
-        file_id = generate_uuid()
         image_files_path = []
+        if not value:
+            logger.warning(f"{self.id}.{key} value is None")
+            return {
+                key_info['key']: all_metadata,
+                key_info['file_content']: all_file_content,
+                key_info['file_path']: original_file_path,
+                key_info['image_file']: image_files_path
+            }
+
+        file_parse_mode = key_info.get('file_parse_mode', ParseModeEnum.INGEST_TO_KNOWLEDGE_BASE)
         file_content_max_size = int(key_info.get('file_content_size', 15000))
+
+        file_id = generate_uuid()
+
         file_content_length = 0
         for one_file_url in value:
-            file_name, file_path, texts, metadatas = self.get_upload_file_path_content(one_file_url)
-            original_file_path.append(file_path)
-            file_ext = file_name.split('.')[-1].lower()
-            if file_ext in self._image_ext:
-                image_files_path.append(file_path)
+            url_obj = urlparse(one_file_url)
+            file_name = unquote(url_obj.path.split('/')[-1])
 
+            file_ext = file_name.split('.')[-1].lower()
+            logger.debug(f"{self.id}.{key} file_parse_mode is keep_raw")
+            original_file_path.append(one_file_url)
+            if file_ext in self._image_ext:
+                image_files_path.append(one_file_url)
+
+            if file_parse_mode == ParseModeEnum.KEEP_RAW:
+                continue
+
+            file_name, file_path, texts, metadatas = self.get_upload_file_path_content(one_file_url)
             if file_content_length < file_content_max_size:
                 file_content = "\n".join(texts)
                 file_content = file_content[:file_content_max_size - file_content_length]
                 file_content_length += len(file_content)
                 all_file_content += f"[file name]: {file_name}\n[file content begin]\n{file_content}\n[file content end]\n"
-
             if not texts:
+                logger.debug(f"{self.id}.{key} extract file text is empty")
+                continue
+            if file_parse_mode == ParseModeEnum.EXTRACT_TEXT:
                 continue
 
+            self.init_vector_clients()
             new_metadata = []
-            # A file corresponding to the same variable, placed in afile_idmile
+            # A file corresponding to the same variable, placed in a file_id mile
             for one in metadatas:
                 metadata = one.model_dump()
                 metadata.update({
@@ -239,18 +237,16 @@ class InputNode(BaseNode):
                 })
                 new_metadata.append(metadata)
 
-            # Uploaded tomilvusAndes
+            # Uploaded to milvus And es
             logger.debug(f'workflow_add_vectordb file={key} file_name={file_name} file_id={file_id}')
-            # Depositmilvus
             self._vector_client.add_texts(texts=texts, metadatas=new_metadata)
 
             logger.debug(f'workflow_add_es file={key} file_name={file_name} file_id={file_id}')
-            # Deposites
             self._es_client.add_texts(texts=texts, metadatas=new_metadata)
 
             logger.debug(f'workflow_record_file_metadata file={key} file_name={file_name}')
             all_metadata.append(new_metadata[0])
-        # Documentationmetadata, other nodes according tometadataData to retrieve corresponding files
+        # Documentation metadata, other nodes according to metadataData to retrieve corresponding files
         return {
             key_info['key']: all_metadata,
             key_info['file_content']: all_file_content,
