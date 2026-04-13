3a03d036cd1ec3462ecd862a36385f15bc229973
alu <rui729907578@163.com>
2026-04-13 15:23:22 +0800

    git commit -m "fixed error"

--- src/backend/bisheng/knowledge/domain/models/knowledge.py
+++ src/backend/bisheng/knowledge/domain/models/knowledge.py
@@ -72,7 +72,7 @@ class KnowledgeBase(SQLModelSerializable):
     create_time: Optional[datetime] = Field(default=None, sa_column=Column(
         DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP')))
     update_time: Optional[datetime] = Field(default=None, sa_column=Column(
-        DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP:wq')))
+        DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP')))
 
     @field_validator('model', mode='before')
     @classmethod
